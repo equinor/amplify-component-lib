48a98e2936f1690eb3672746d77bd47407bc2f1e
MathiasHartveit <mhart@equinor.com>
2021-06-07 15:48:09 +0200

    FilterMenu component

---
+++ src/stories/FilterMenu/FilterMenu.stories.tsx
@@ -0,0 +1,19 @@
+import { filter_list } from "@equinor/eds-icons";
+import { Story, Meta } from "@storybook/react";
+
+import FilterMenu, { IComponentProps } from ".";
+
+export default {
+  title: "FilterMenu",
+  component: FilterMenu,
+  argTypes: {
+    menuTitle: { defaultValue: "Menu Title" },
+    data: { defaultValue: ["Item 1", "Item 2", "Apple"] },
+    icon: { defaultValue: filter_list },
+  },
+} as Meta;
+
+const Template: Story<IComponentProps> = (args) => <FilterMenu {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {};
