798fbb86b8b3fc689663714fb91527c1d8a7a48f
Mathih13 <mhartveit61@gmail.com>
2021-10-15 14:51:51 +0200

    :children_crossing: Added withoutScrim functionality to spinner

--- src/components/FullPageSpinner/index.tsx
+++ src/components/FullPageSpinner/index.tsx
@@ -1,16 +1,45 @@
 import { Progress, Scrim, StarProgress } from '@equinor/eds-core-react';
+import styled from 'styled-components';
 
 interface IFullpageSpinnerProps {
   variant?: 'equinor' | 'circle' | 'dots';
+  withoutScrim?: boolean;
 }
 
-const FullPageSpinner: React.FC<IFullpageSpinnerProps> = ({ variant }) => {
+const NoScrimContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  z-index: 500;
+  top: 0;
+  left: 0;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+`;
+
+const FullPageSpinner: React.FC<IFullpageSpinnerProps> = ({
+  variant,
+  withoutScrim,
+}) => {
+  const renderSpinner = () => {
+    return (
+      <>
+        {(variant === 'equinor' || !variant) && <StarProgress />}
+        {variant === 'circle' && <Progress.Circular />}
+        {variant === 'dots' && <Progress.Dots />}
+      </>
+    );
+  };
+
   return (
-    <Scrim>
-      {(variant === 'equinor' || !variant) && <StarProgress />}
-      {variant === 'circle' && <Progress.Circular />}
-      {variant === 'dots' && <Progress.Dots />}
-    </Scrim>
+    <>
+      {withoutScrim ? (
+        <NoScrimContainer>{renderSpinner()}</NoScrimContainer>
+      ) : (
+        <Scrim>{renderSpinner()}</Scrim>
+      )}
+    </>
   );
 };
 
