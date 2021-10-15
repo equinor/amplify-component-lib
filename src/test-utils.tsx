fc28e8014ce4a4f761805004a3b873a6acd4d227
Mathih13 <mhartveit61@gmail.com>
2021-10-15 14:29:19 +0200

    :adhesive_bandage: Fixed strange typing with test utils

--- src/test-utils.tsx
+++ src/test-utils.tsx
@@ -1,14 +1,9 @@
 // test-utils.js
-import { render, RenderResult } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
-const customRender: (
-  ui: React.ReactElement,
-  options?: any
-) => RenderResult<
-  typeof import('c:/Users/mhart/source/repos/equinor/amplify-components/node_modules/@testing-library/react/node_modules/@testing-library/dom/types/queries'),
-  HTMLElement
-> = (ui: React.ReactElement, options?: any) => render(ui, { ...options });
+const customRender = (ui: React.ReactElement, options?: any) =>
+  render(ui, { ...options });
 
 // re-export everything
 export * from '@testing-library/react';
