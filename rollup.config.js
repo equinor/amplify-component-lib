147ac138973b4b80709624dc0ce1a04b97070925
Mathih13 <mhartveit61@gmail.com>
2021-10-26 18:59:17 +0200

    :sparkles: Moved build to Rollup

---
+++ rollup.config.js
@@ -0,0 +1,38 @@
+/* eslint-disable import/no-default-export */
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
+import typescript from 'rollup-plugin-typescript2';
+import { typescriptPaths } from 'rollup-plugin-typescript-paths';
+
+import pkg from './package.json';
+
+const globals = {
+  react: 'React',
+  'react-dom': 'ReactDOM',
+  'styled-components': 'styled',
+};
+
+const peerDeps = Object.keys(pkg.peerDependencies || {});
+const extensions = ['.jsx', '.js', '.tsx', '.ts'];
+
+export default [
+  {
+    input: './src/index.ts',
+    external: peerDeps,
+    plugins: [
+      resolve({ extensions }),
+      typescript({ useTsconfigDeclarationDir: true }),
+      typescriptPaths(),
+      babel({
+        exclude: 'node_modules/**',
+        babelHelpers: 'bundled',
+        presets: ['@babel/preset-env', '@babel/preset-react'],
+        extensions,
+        plugins: ['babel-plugin-styled-components'],
+      }),
+      commonjs(),
+    ],
+    output: [{ file: pkg.main, format: 'umd', name: pkg.name, globals }],
+  },
+];
