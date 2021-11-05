5472a0cb8d488da0bfb8c69c9c5c2e02eb1dd0dd
Mathih13 <mhartveit61@gmail.com>
2021-11-05 11:22:24 +0100

    :white_check_mark: Fixed copytext test issue

--- src/components/CopyText/__tests__/index.tsx
+++ src/components/CopyText/__tests__/index.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '../../../utils/test-utils';
+import { render, screen } from '../../../test-utils';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom/extend-expect';
 import CopyText from '../index';
