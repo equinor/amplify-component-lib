8608b86f89210f3cd9fe20bc0e6f2f363cd012bc
Marius Hauge Tobiassen <marius@mh2.no>
2022-09-22 16:18:58 +0200

    :recycle: Refactored AuthProvider so it uses the @azure/msal-react package

--- src/utils/auth_environment.ts
+++ src/utils/auth_environment.ts
@@ -1,6 +1,8 @@
 import {
   PublicClientApplication,
   InteractionRequiredAuthError,
+  IPublicClientApplication,
+  AccountInfo,
 } from '@azure/msal-browser';
 
 interface IConfig {
@@ -70,8 +72,12 @@ const getApiScope = (): string => {
   }
 };
 
-const fetchMsGraph = async (url: string, accessToken: string) => {
-  return await fetch(url, {
+const GRAPH_ENDPOINTS = {
+  PHOTO: 'https://graph.microsoft.com/v1.0/me/photos/96x96/$value',
+};
+
+const fetchMsGraph = (url: string, accessToken: string) => {
+  return fetch(url, {
     headers: {
       Authorization: `Bearer ${accessToken}`,
     },
@@ -97,10 +103,6 @@ const GRAPH_REQUESTS = {
   },
 };
 
-const GRAPH_ENDPOINTS = {
-  PHOTO: 'https://graph.microsoft.com/v1.0/me/photos/96x96/$value',
-};
-
 const msalApp = new PublicClientApplication({
   auth: {
     clientId: getClientId(),
@@ -115,34 +117,41 @@ const msalApp = new PublicClientApplication({
   },
 });
 
-const acquireToken = async (request = GRAPH_REQUESTS.LOGIN) => {
-  return await msalApp
-    .acquireTokenSilent({
-      ...request,
-      redirectUri: `${window.location.origin}/auth.html`,
-    })
-    .then((token) => {
-      if (!token?.accessToken) {
-        console.log(`Token acquire is empty: ${JSON.stringify(token)}`);
-        msalApp.acquireTokenRedirect(request);
-        throw new Error('Redirecting');
-      }
-      return token;
-    })
-    //Acquire token silent failure, and send an interactive request
-    .catch((error) => {
-      console.log(`Token acquire error: ${JSON.stringify(error)}`);
-      localStorage.clear();
-      if (
-        InteractionRequiredAuthError.isInteractionRequiredError(error.errorCode)
-      ) {
-        msalApp.acquireTokenRedirect(request);
-        throw new Error('Redirecting');
-      } else {
-        console.error(`Non-interactive error: ${error.errorCode}`);
-        throw new Error('Reloading');
-      }
-    });
+const acquireToken = async (
+  instance: IPublicClientApplication,
+  request = GRAPH_REQUESTS.LOGIN
+) => {
+  return (
+    instance
+      .acquireTokenSilent({
+        ...request,
+        redirectUri: `${window.location.origin}/auth.html`,
+      })
+      .then((token) => {
+        if (!token?.accessToken) {
+          console.log(`Token acquire is empty: ${JSON.stringify(token)}`);
+          msalApp.acquireTokenRedirect(request);
+          throw new Error('Redirecting');
+        }
+        return token;
+      })
+      //Acquire token silent failure, and send an interactive request
+      .catch((error) => {
+        console.log(`Token acquire error: ${JSON.stringify(error)}`);
+        localStorage.clear();
+        if (
+          InteractionRequiredAuthError.isInteractionRequiredError(
+            error.errorCode
+          )
+        ) {
+          msalApp.acquireTokenRedirect(request);
+          throw new Error('Redirecting');
+        } else {
+          console.error(`Non-interactive error: ${error.errorCode}`);
+          throw new Error('Reloading');
+        }
+      })
+  );
 };
 
 const isReaderOnly = (roles: string[] | undefined) => {
