f84aad59b2cbf23b96e705223b0ce764d0443101
Marius Tobiassen <24507952+mariush2@users.noreply.github.com>
2022-10-20 07:45:19 +0200

    Update env.sh

--- config/config_files/env.sh
+++ config/config_files/env.sh
@@ -21,6 +21,4 @@ echo "  API_SCOPE: \"$value\"," >>./env-config.js
 echo "}" >>./env-config.js
 
 # Update nginx.conf with relevant environment variables
-envsubst '
-  ${API_URL}
-  ' </etc/nginx/conf.d/default.conf.template >/etc/nginx/conf.d/default.conf
+envsubst '${API_URL}' </etc/nginx/conf.d/default.conf.template >/etc/nginx/conf.d/default.conf
