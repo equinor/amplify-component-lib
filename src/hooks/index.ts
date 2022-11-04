3571458fc99246789f5fe86c7b5bff8b7e4e2735
ReihanehShahali <reihanehshl@gmail.com>
2022-11-04 11:35:21 +0100

    add tutorial hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,7 +1,15 @@
 import { useAuth } from '../providers/AuthProvider/AuthProvider';
 import { useSideBar } from '../providers/SideBarProvider';
 import { useSnackbar } from '../providers/SnackbarProvider';
+import { useTutorialSteps } from '../providers/TutorialStepsProvider';
 import { useDebounce } from './useDebounce';
 import { useOnScreen } from './useOnScreen';
 
-export { useAuth, useDebounce, useOnScreen, useSideBar, useSnackbar };
+export {
+  useAuth,
+  useDebounce,
+  useOnScreen,
+  useSideBar,
+  useSnackbar,
+  useTutorialSteps,
+};
