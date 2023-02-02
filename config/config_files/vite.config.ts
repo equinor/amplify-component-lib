496a835aef8a1292ac83bd3fd24ba8bb9fe4e52d
Marius Tobiassen <24507952+mariush2@users.noreply.github.com>
2023-02-02 07:49:58 +0100

    Removed vite-plugin-svgr from vite.config.ts

--- config/config_files/vite.config.ts
+++ config/config_files/vite.config.ts
@@ -4,7 +4,6 @@
 import react from '@vitejs/plugin-react';
 
 import { defineConfig, loadEnv } from 'vite';
-import svgrPlugin from 'vite-plugin-svgr';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
@@ -12,7 +11,7 @@ export default ({ mode }: { mode: string }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
   return defineConfig({
-    plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+    plugins: [react(), viteTsconfigPaths()],
     server: {
       open: true,
       port: 3000,
