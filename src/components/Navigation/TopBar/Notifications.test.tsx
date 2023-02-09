630ad01b684bb7f3f52fb1b8ed9c8fe53e7d9687
aslakihle <aslak.ihle@gmail.com>
2023-02-09 12:14:57 +0100

    :white_check_mark: fixed test for notifications component

--- src/components/Navigation/TopBar/Notifications.test.tsx
+++ src/components/Navigation/TopBar/Notifications.test.tsx
@@ -3,17 +3,16 @@ import React from 'react';
 import { notifications } from '@equinor/eds-icons';
 import { faker } from '@faker-js/faker';
 
-import { expect, render, screen, test, userEvent } from '../../../test-utils';
+import { render, screen, userEvent } from '../../../tests/test-utils';
 import Notifications from './Notifications';
 
 test('renders button and panel correctly', async () => {
   render(<Notifications setAllAsRead={() => null} />);
   const icons = screen.getAllByTestId('eds-icon-path');
-  const panelElement =
-    screen.queryByText('Notifications')?.parentElement?.parentElement;
+
   expect(icons[0]).toHaveAttribute('d', notifications.svgPathData);
-  console.log(panelElement?.style.display);
-  expect(panelElement).toBeVisisble();
+  expect(screen.queryByText('Notifications')).not.toBeVisible();
+
   const button = screen.getByRole('button');
   await userEvent.click(button);
   expect(screen.getByText('Notifications')).toBeVisible();
