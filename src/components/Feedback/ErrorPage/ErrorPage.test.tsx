53fd64a5253e197b8359025c3dba1d215b8bfdce
Marius Hauge Tobiassen <marius@mh2.no>
2023-03-21 09:46:47 +0100

    :white_check_mark: Improved coverage for ErrorPage.test.tsx

--- src/components/Feedback/ErrorPage/ErrorPage.test.tsx
+++ src/components/Feedback/ErrorPage/ErrorPage.test.tsx
@@ -21,6 +21,22 @@ test('Shows default values without props', () => {
   );
 });
 
+test('Render given illustration', () => {
+  const defaultError = getErrorContent('Amplify portal', ErrorType.DEFAULT);
+
+  render(
+    <ErrorPage illustration={defaultError.illustration}>
+      <ErrorPage.Title />
+      <ErrorPage.Description />
+      <ErrorPage.Action />
+    </ErrorPage>
+  );
+  expect(screen.getByTestId('title')).toHaveTextContent(defaultError.title);
+  expect(screen.getByTestId('description')).toHaveTextContent(
+    defaultError.description ?? ''
+  );
+});
+
 test('Shows details after clicking on More details.', async () => {
   const error500 = getErrorContent(
     'Amplify portal',
@@ -41,4 +57,33 @@ test('Shows details after clicking on More details.', async () => {
   await user.click(showDetailsButton);
 
   expect(screen.getByTestId('details-text')).toBeInTheDocument();
+});
+
+test('Shows missing access when 401 error', async () => {
+  const error401 = getErrorContent('Amplify portal', ErrorType.ERROR_401);
+  const missingAccess = new Array(faker.datatype.number({ min: 1, max: 5 }))
+    .fill(0)
+    .map(() => ({
+      title: faker.commerce.productName(),
+      url: faker.internet.url(),
+    }));
+
+  render(
+    <ErrorPage illustration={error401.illustration}>
+      <ErrorPage.Title title={error401.title} />
+      <ErrorPage.Description text={error401.description} />
+      <ErrorPage.Action buttonText={error401.button?.text} />
+      <ErrorPage.MissingAccesses accesses={missingAccess} />
+    </ErrorPage>
+  );
+  window.open = vi.fn();
+  const user = userEvent.setup();
+
+  for (const access of missingAccess) {
+    expect(screen.getByText(access.title)).toBeInTheDocument();
+    await user.click(
+      screen.getByTestId(`missing-access-button-${access.title}`)
+    );
+    expect(window.open).toHaveBeenCalledWith(access.url, '_blank');
+  }
 });
