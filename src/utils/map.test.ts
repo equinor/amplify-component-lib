2fdabf0321c09167f1bed7b8d369b287c624dbf3
Marius Hauge Tobiassen <marius@mh2.no>
2023-03-21 08:39:40 +0100

    :white_check_mark: Added coverage for map.test

--- src/utils/map.test.ts
+++ src/utils/map.test.ts
@@ -20,3 +20,26 @@ test('distanceLatLng works as expected', () => {
     })
   ).toBeLessThanOrEqual(10);
 });
+
+test('convertUtmToLatLng works as expected', () => {
+  const utmCoordinate = {
+    x: 431400,
+    y: 6537900,
+  };
+  const converted = map.convertUtmToLatLng(utmCoordinate.x, utmCoordinate.y);
+
+  const expectedLatLng = { lat: 58.973227394399274, lng: 1.8052774673154197 };
+
+  expect(converted).toStrictEqual(expectedLatLng);
+});
+
+test('convertLatLngToUtm works as expected', () => {
+  const latLng = { lat: 58.973227394399274, lng: 1.8052774673154197 };
+  const converted = map.convertLatLngToUtm(latLng);
+
+  const expectedUtm = [431400, 6537900];
+
+  /// The conversion includes a lot of decimals which is why we round it to the nearest whole number
+  expect(Math.round(converted[0])).toBe(expectedUtm[0]);
+  expect(Math.round(converted[1])).toBe(expectedUtm[1]);
+});
