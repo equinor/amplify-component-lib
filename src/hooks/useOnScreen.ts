1f6446a5590b2f037ea2259ef8c383e63e485bcf
Marius Tobiassen Bungum <marius@mh2.no>
2023-04-05 13:10:09 +0200

    :technologist: Added usePrevious and useOnScreenMultiple hooks

--- src/hooks/useOnScreen.ts
+++ src/hooks/useOnScreen.ts
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 
 // https://stackoverflow.com/a/65008608
 export function useOnScreen(
-  ref: { current: HTMLDivElement | null },
+  ref: { current: HTMLElement | null },
   threshold: number | number[] | undefined
 ) {
   const [isIntersecting, setIntersecting] = useState(false);
@@ -21,6 +21,37 @@ export function useOnScreen(
       observer.disconnect();
     };
   }, [ref, threshold]);
+
+  return isIntersecting;
+}
+
+export function useOnScreenMultiple(elements: (Element | null)[]) {
+  const [isIntersecting, setIntersecting] = useState<boolean[]>(
+    new Array(elements.length).fill(false)
+  );
+
+  useEffect(() => {
+    const observers: IntersectionObserver[] = [];
+    for (const [index, element] of elements.entries()) {
+      if (element === null) continue;
+      const observer = new IntersectionObserver(([entry]) => {
+        setIntersecting((current) => {
+          const newIntersecting = [...current];
+          newIntersecting[index] = entry.isIntersecting;
+          return newIntersecting;
+        });
+      });
+
+      observer.observe(element);
+
+      observers.push(observer);
+    }
+    return () => {
+      for (const observer of observers) {
+        observer.disconnect();
+      }
+    };
+  }, [elements]);
 
   return isIntersecting;
 }
