698823fb386f45d1dc68ffde673c68b9f0690bfa
ReihanehShahali <reihanehshl@gmail.com>
2023-04-27 08:55:32 +0200

    changed questioning to be smoother

--- src/components/Feedback/ErrorPage/illustrations/QuestioningAnimation.tsx
+++ src/components/Feedback/ErrorPage/illustrations/QuestioningAnimation.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 
 import styled, { keyframes } from 'styled-components';
 const animation = keyframes`
-  100% { background-position: calc(-18942px/2); }
+  100% { background-position: calc(-19516px/2); }
 `;
 const Container = styled.div`
   width: 400px;
@@ -11,8 +11,8 @@ const Container = styled.div`
 `;
 
 const Animation = styled.div`
-  height: 307px; // 614 / 2 size of the image
-  width: calc(18942px / (2 * 33));
+  height: 307px;
+  width: calc(19516px / (2 * 34));
   position: absolute;
   top: 50%;
   left: 50%;
@@ -20,7 +20,7 @@ const Animation = styled.div`
   background: url('https://raw.githubusercontent.com/equinor/amplify-components/main/static/Questioning_spritesheet.png')
     left center;
   background-size: cover;
-  animation: ${animation} 2s steps(33) infinite;
+  animation: ${animation} 2.5s steps(34) infinite;
 `;
 const QuestioningAnimation: FC = () => {
   return (
