7486fae4ba09f3def5edb24dd6c3a7985b9b4094
Marius Tobiassen Bungum <marius@mh2.no>
2023-05-23 07:21:01 +0200

    :package: Added application icons (IconData type) to export

--- src/components/index.ts
+++ src/components/index.ts
@@ -34,6 +34,18 @@ export {
 export { default as DataAcquisition } from './Icons/ApplicationIcon/Acquire';
 export type { ApplicationName } from './Icons/ApplicationIcon/ApplicationIcon';
 export { default as ApplicationIcon } from './Icons/ApplicationIcon/ApplicationIcon';
+export {
+  acquire,
+  dasha,
+  depthConversion,
+  fallback,
+  fourDInsight,
+  inPress,
+  loggingQualification,
+  portal,
+  pwex,
+  recap,
+} from './Icons/ApplicationIcon/ApplicationIconCollection';
 export { default as DataSharing } from './Icons/ApplicationIcon/Dasha';
 export { default as DataExperience } from './Icons/ApplicationIcon/FourDInsight';
 export { default as Portal } from './Icons/ApplicationIcon/Portal';
