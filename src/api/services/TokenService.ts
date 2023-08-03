660e12a3be70f7b64f0366f50399453f5d35cfe0
aslakihle <aslak.ihle@gmail.com>
2023-08-03 14:20:02 +0200

    :alien: Improving fake services and OpenAPI config
    Signed-off-by: aslakihle <aslak.ihle@gmail.com>

--- src/api/services/TokenService.ts
+++ src/api/services/TokenService.ts
@@ -7,23 +7,6 @@ import { request as __request } from '../core/request';
 
 export class TokenService {
   /**
-   * Gets a token for the application with the provided clientId
-   * @param clientId
-   * @returns string Success
-   * @throws ApiError
-   */
-  public static getToken(clientId: string): CancelablePromise<string> {
-    return __request(OpenAPI, {
-      method: 'GET',
-      url: '/api/v1/Token/{clientId}',
-      path: {
-        clientId: clientId,
-      },
-    });
-  }
-
-  /**
-   * Gets a Token for Amplify Portal for the Current Radix Environemnt (development/staging/production)
    * @returns string Success
    * @throws ApiError
    */
@@ -35,7 +18,6 @@ export class TokenService {
   }
 
   /**
-   * Gets a token for Amplify Portal in Production
    * @returns string Success
    * @throws ApiError
    */
