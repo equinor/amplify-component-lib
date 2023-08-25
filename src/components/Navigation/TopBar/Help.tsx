26c76bbf0369d6504d168e4eba20050dd411652b
Birte Jelsa <birte.jelsa@gmail.com>
2023-08-25 14:20:28 +0200

    ✨ Ability to include custom help menu items

--- src/components/Navigation/TopBar/Help.tsx
+++ src/components/Navigation/TopBar/Help.tsx
@@ -1,6 +1,6 @@
-import React, { FC, useRef, useState } from 'react';
+import { forwardRef, ReactNode, useRef, useState } from 'react';
 
-import { Button, Icon, Typography } from '@equinor/eds-core-react';
+import { Button, Divider, Icon, Typography } from '@equinor/eds-core-react';
 import {
   external_link,
   file_description,
@@ -26,70 +26,95 @@ const MenuLinkItem = styled.a`
   }
 `;
 
+const ContentWrapper = styled.div`
+  padding: 0 ${spacings.comfortable.medium};
+`;
+
 const ContentInfo = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
   gap: ${spacings.comfortable.medium_small};
-  padding-right: 50px;
+`;
+
+const Spacer = styled.div`
+  display: flex;
 `;
 
 export interface HelpProps {
   applicationName: string;
+  hideDefaultMenuItems?: boolean;
+  children?: ReactNode;
 }
 
-export const Help: FC<HelpProps> = ({ applicationName }) => {
-  const [isOpen, setIsOpen] = useState(false);
-  const buttonRef = useRef<HTMLDivElement | null>(null);
-  const closeMenu = () => setIsOpen(false);
-  const toggleMenu = () => setIsOpen(!isOpen);
+export const Help = forwardRef<HTMLDivElement, HelpProps>(
+  ({ applicationName, hideDefaultMenuItems = false, children }, ref) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const buttonRef = useRef<HTMLDivElement | null>(null);
+    const closeMenu = () => setIsOpen(false);
+    const toggleMenu = () => setIsOpen(!isOpen);
 
-  return (
-    <>
-      <Button
-        variant="ghost_icon"
-        ref={buttonRef}
-        id="anchor-match"
-        aria-haspopup="true"
-        aria-expanded={isOpen}
-        aria-controls="menu-match"
-        onClick={toggleMenu}
-      >
-        <Icon
-          data={help_outline}
-          color={colors.interactive.primary__resting.hsla}
-        />
-      </Button>
-
-      <TopBarMenu
-        open={isOpen}
-        title="Help"
-        onClose={closeMenu}
-        anchorEl={buttonRef.current}
-      >
-        <MenuLinkItem
-          as="a"
-          onClick={closeMenu}
-          href={`https://amplify.equinor.com/releasenotes?app=%5B"${applicationName}"%5D`}
-          target="_blank"
+    return (
+      <>
+        <Button
+          variant="ghost_icon"
+          ref={buttonRef}
+          id="anchor-match"
+          aria-haspopup="true"
+          aria-expanded={isOpen}
+          aria-controls="menu-match"
+          onClick={toggleMenu}
         >
-          <ContentInfo>
-            <Icon
-              data={file_description}
-              size={24}
-              color={colors.interactive.primary__resting.hsla}
-            />
-            <Typography group="navigation" variant="menu_title" as="span">
-              Release notes
-            </Typography>
-          </ContentInfo>
           <Icon
-            data={external_link}
-            size={24}
+            data={help_outline}
             color={colors.interactive.primary__resting.hsla}
           />
-        </MenuLinkItem>
-      </TopBarMenu>
-    </>
-  );
-};
+        </Button>
+
+        <TopBarMenu
+          open={isOpen}
+          title="Help"
+          onClose={closeMenu}
+          anchorEl={buttonRef.current}
+          ref={ref}
+        >
+          {!hideDefaultMenuItems && (
+            <MenuLinkItem
+              as="a"
+              onClick={closeMenu}
+              href={`https://amplify.equinor.com/releasenotes?app=%5B"${applicationName}"%5D`}
+              target="_blank"
+            >
+              <ContentInfo>
+                <Icon
+                  data={file_description}
+                  size={24}
+                  color={colors.interactive.primary__resting.hsla}
+                />
+                <Typography group="navigation" variant="menu_title" as="span">
+                  Release notes
+                </Typography>
+              </ContentInfo>
+              <Spacer />
+              <Icon
+                data={external_link}
+                size={24}
+                color={colors.interactive.primary__resting.hsla}
+              />
+            </MenuLinkItem>
+          )}
+          {children && !hideDefaultMenuItems && (
+            <Divider style={{ margin: 0 }} />
+          )}
+          {children && (
+            <ContentWrapper onClick={() => closeMenu()}>
+              {children}
+            </ContentWrapper>
+          )}
+        </TopBarMenu>
+      </>
+    );
+  }
+);
+
+Help.displayName = 'Help';
