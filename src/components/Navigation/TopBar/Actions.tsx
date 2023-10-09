183c40f8e70706e9f8bc3f4c998840a9a4fb93b4
Brynjulf Risbakken <brynjulf.risbakken@olavstoppen.no>
2023-10-09 16:01:03 +0200

    :recycle: use eds token for spacing instead of hardcoded value

--- src/components/Navigation/TopBar/Actions.tsx
+++ src/components/Navigation/TopBar/Actions.tsx
@@ -1,14 +1,17 @@
 import { forwardRef, HTMLAttributes } from 'react';
 
 import { TopBar as EDSTopBar } from '@equinor/eds-core-react';
+import { tokens } from '@equinor/eds-tokens';
 
 import styled from 'styled-components';
+
+const { spacings } = tokens;
 
 const ActionsContainer = styled(EDSTopBar.Actions)`
   display: flex;
   align-items: center;
   flex-direction: row-reverse;
-  gap: 24px;
+  gap: ${spacings.comfortable.large};
 `;
 
 export const Actions = forwardRef<
