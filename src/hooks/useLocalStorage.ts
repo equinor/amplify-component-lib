ec765514ee52a2d059b19c4fe93df668f0b911df
aslakihle <aslak.ihle@gmail.com>
2023-11-08 12:02:35 +0100

    :sparkles: added keepAlive prop to useLocalStorage hook

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,9 +1,25 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useState } from 'react';
 
-export const getLocalStorage = <T>(key: string, defaultState: T): T => {
+import { debounce } from 'lodash';
+
+const LAST_UPDATED_KEY_SUFFIX = '-last-updated';
+
+export const getLocalStorage = <T>(
+  key: string,
+  defaultState: T,
+  keepAlive?: number
+): T => {
   const localStorageData = localStorage.getItem(key);
+  const localStorageLastEdited = localStorage.getItem(
+    key + LAST_UPDATED_KEY_SUFFIX
+  );
 
-  if (localStorageData) {
+  if (
+    localStorageData &&
+    (!keepAlive ||
+      (localStorageLastEdited &&
+        Number(localStorageLastEdited) + keepAlive > new Date().getTime()))
+  ) {
     return JSON.parse(localStorageData);
   }
 
@@ -14,14 +30,29 @@ export const updateLocalStorage = <T>(key: string, state: T) => {
   localStorage.setItem(key, JSON.stringify(state));
 };
 
-export const useLocalStorage = <T>(key: string, defaultState: T) => {
-  const [state, setState] = useState<T>(getLocalStorage<T>(key, defaultState));
+export const useLocalStorage = <T>(
+  key: string,
+  defaultState: T,
+  keepAlive?: number
+) => {
+  const [state, setState] = useState<T>(
+    getLocalStorage<T>(key, defaultState, keepAlive)
+  );
 
-  useEffect(() => {
-    if (state !== undefined) {
+  const debouncedUpdateLocalStorage = useCallback((key: string, state: T) => {
+    return debounce(() => {
       updateLocalStorage(key, state);
-    }
-  }, [key, state]);
+      updateLocalStorage(key + LAST_UPDATED_KEY_SUFFIX, new Date().getTime());
+    }, 1000);
+  }, []);
 
-  return [state, setState] as const;
+  const handleSetState = (state: T) => {
+    setState(state);
+    if (state !== undefined) {
+      const test = debouncedUpdateLocalStorage(key, state);
+      test();
+    }
+  };
+
+  return [state, handleSetState] as const;
 };
