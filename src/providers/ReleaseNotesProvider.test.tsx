47a65223531c4b287f9bb587ff3eff95c53e4674
Brynjulf Risbakken <brynjulf.risbakken@olavstoppen.no>
2023-11-09 08:27:39 +0100

    :white_check_mark: updated release notes provider test with necessary providers

--- src/providers/ReleaseNotesProvider.test.tsx
+++ src/providers/ReleaseNotesProvider.test.tsx
@@ -1,9 +1,22 @@
+import { FC } from 'react';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 import { renderHook } from '../tests/test-utils';
 import ReleaseNotesProvider, { useReleaseNotes } from './ReleaseNotesProvider';
 
+const Wrappers: FC<{ children: any }> = ({ children }) => {
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReleaseNotesProvider>{children}</ReleaseNotesProvider>
+    </QueryClientProvider>
+  );
+};
+
 test('open = false works as expected', () => {
   const { result } = renderHook(() => useReleaseNotes(), {
-    wrapper: ReleaseNotesProvider,
+    wrapper: Wrappers,
   });
 
   expect(result.current.open).toBe(false);
