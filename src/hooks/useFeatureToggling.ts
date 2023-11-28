db31b7694b8607148fcc77bc611ff4a692961afd
aslakihle <aslak.ihle@gmail.com>
2023-11-28 14:11:16 +0100

    :sparkles: Added FeatureToggleProvider to load every relevant featureToggle on first render

--- src/hooks/useFeatureToggling.ts
+++ src/hooks/useFeatureToggling.ts
@@ -1,43 +1,21 @@
 import { useMemo } from 'react';
 
-import { useQuery } from '@tanstack/react-query';
-
-import { FeatureToggleDto, GraphUser } from 'src/api';
-import { PortalService } from 'src/api/services/PortalService';
+import {
+  isUserInActiveUserArray,
+  useFeatureToggleContext,
+} from '../providers/FeatureToggleProvider';
 import { useAuth } from 'src/providers/AuthProvider/AuthProvider';
 import { environment } from 'src/utils';
 
-const { getAppName, getEnvironmentName } = environment;
+const { getEnvironmentName } = environment;
 
 export function useFeatureToggling(featureKey: string) {
   const { account } = useAuth();
   const username = `${account?.username}`;
-  const applicationName = getAppName(import.meta.env.VITE_NAME);
   const environment = getEnvironmentName(import.meta.env.VITE_ENVIRONMENT_NAME);
+  const { features, isError } = useFeatureToggleContext();
 
-  const isUserInActiveUserArray = (
-    username: string,
-    activeUsers: GraphUser[] | undefined | null
-  ) => {
-    if (activeUsers && activeUsers.length > 0) {
-      return activeUsers.filter((user) =>
-        (user.mail ?? '').toLowerCase().includes(username.toLowerCase())
-      );
-    }
-    return false;
-  };
-
-  const {
-    data: featureToggle,
-    isLoading,
-    isError,
-  } = useQuery<FeatureToggleDto>({
-    queryKey: ['getFeatureToggleFromAppName'],
-    queryFn: async () =>
-      PortalService.getFeatureToggleFromApplicationName(applicationName),
-  });
-
-  const feature = featureToggle?.features?.find(
+  const feature = features?.find(
     (feature) => feature.featureKey === featureKey
   );
 
@@ -45,9 +23,13 @@ export function useFeatureToggling(featureKey: string) {
     if (feature) {
       if (isUserInActiveUserArray(username, feature.activeUsers)) {
         return true;
-      } else return feature.activeEnvironments?.includes(environment);
-    } else return isLoading ? false : !isError;
-  }, [environment, feature, isError, isLoading, username]);
+      } else if (feature.activeEnvironments) {
+        return feature.activeEnvironments.includes(environment);
+      } else {
+        return false;
+      }
+    } else return !isError;
+  }, [environment, feature, isError, username]);
 
-  return { showContent, isLoading };
+  return { showContent };
 }
