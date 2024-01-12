10cb6ab3203400e03502e4f6d243d81f8b0dbc6c
Marius Tobiassen Bungum <marius@mh2.no>
2024-01-12 11:24:34 +0100

    :adhesive_bandage: Fixed issue with logging when preset app didn't match anything

--- tooling/build-and-use.mjs
+++ tooling/build-and-use.mjs
@@ -1,84 +1,129 @@
 import { runTask } from './taskrunner.mjs';
 import chalk from 'chalk';
-import { readdir } from "fs/promises";
-import * as readline from 'node:readline/promises'
-import { stdin as input, stdout as output, } from 'node:process'
+import { readdir } from 'fs/promises';
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
 
 async function runTasks() {
-    console.clear()
-    const presetApp = process.argv.at(2).slice(2)
-    if (presetApp) {
-        console.log(chalk.bold.yellow('Running build and use tasks with ')
-            + chalk.bold.magentaBright(presetApp)
-            + chalk.bold.yellow(' as preset app...\n'))
-    } else {
-        console.log(chalk.bold.yellow('Running build and use tasks...\n'))
-    }
+  console.clear();
+  const presetApp = process.argv.at(2).match(/--(\w+-?)+/)?.[1];
+  if (presetApp) {
+    console.log(
+      chalk.bold.yellow('Running build and use tasks with ') +
+        chalk.bold.magentaBright(presetApp) +
+        chalk.bold.yellow(' as preset app...\n')
+    );
+  } else {
+    console.log(chalk.bold.yellow('Running build and use tasks...\n'));
+  }
 
-
-    try {
-        await runTask({
-            command: 'yarn build',
-            name: `${chalk.white('Building')} ${chalk.bold.magenta('amplify-components')}`,
-            ignoreStdErr: true
-        })
-    } catch(err) {
-        console.error(err)
-        return
-    }
-
-    const dirents = await readdir('..', { withFileTypes: true })
-    const dirs = dirents.filter(dirent => dirent.isDirectory()).map((dirent) => dirent.name)
-
-    const rl = readline.createInterface({ input, output });
-
-    let selectedDir = undefined
-    let failed = false
-
-    if (dirs.find((dir) => presetApp.toLowerCase() === dir.toLowerCase())) {
-        selectedDir = presetApp
-    } else {
-        console.log(chalk.red('Unable to find directory with the preset name: ') + chalk.greenBright.bold(presetApp))
-    }
-
-    while (selectedDir === undefined) {
-        console.clear()
-        console.log(chalk.bold('Found these directories in parent folder:'))
-        console.log(chalk.bold.blue(dirs.join('\n')))
-        if (failed) {
-            console.log(chalk.red('Unable to find a directory with that name! Please try again...'))
-        }
-        const answer = await rl.question(chalk.greenBright('Write name of project you want to copy local amplify-components into:') + `\n> `)
-        selectedDir = dirs.find((dir) => dir.toLowerCase() === answer.toLowerCase())
-        if (selectedDir === undefined) {
-            failed = true
-        }
-    }
-
-    console.clear()
-    console.log(chalk.greenBright(`${chalk.bold.magentaBright(selectedDir)} has been selected.`))
-
+  try {
     await runTask({
-        command: `rm -rf ../${selectedDir}/client/node_modules/.vite`,
-        name: `Removing ${chalk.bold.greenBright('.vite')} folder from ${chalk.bold.greenBright(`${selectedDir}/client/node_modules`)}`
-    })
+      command: 'yarn build',
+      name: `${chalk.white('Building')} ${chalk.bold.magenta(
+        'amplify-components'
+      )}`,
+      ignoreStdErr: true,
+    });
+  } catch (err) {
+    console.error(err);
+    return;
+  }
 
-    await runTask({
-        command: `rm -rf ../${selectedDir}/client/node_modules/.cache`,
-        name: `Removing ${chalk.bold.greenBright('.cache')} folder from ${chalk.bold.greenBright(`${selectedDir}/client/node_modules`)}`
-    })
+  const dirents = await readdir('..', { withFileTypes: true });
+  const dirs = dirents
+    .filter((dirent) => dirent.isDirectory())
+    .map((dirent) => dirent.name);
 
-    await runTask({
-        command: `rm -rf ../${selectedDir}/client/node_modules/@equinor/amplify-components/dist`,
-        name: `Removing old ${chalk.bold.greenBright('amplify-components/dist')} folder from ${chalk.bold.greenBright(`${selectedDir}/client/node_modules`)}`
-    })
+  const rl = readline.createInterface({ input, output });
 
-    await runTask({
-        command: `cp -r ./dist ../${selectedDir}/client/node_modules/@equinor/amplify-components/dist`,
-        name: `Copying newly built ${chalk.bold.greenBright('dist')} folder into ${chalk.bold.greenBright(`${selectedDir}/client/node_modules`)}`
-    })
+  let selectedDir = undefined;
+  let failed = false;
 
-    console.log(chalk.green.bold(`Built new version and copied it to ${chalk.bold.magentaBright(selectedDir)}! 🎉`))
+  console.clear();
+
+  if (dirs.find((dir) => presetApp.toLowerCase() === dir.toLowerCase())) {
+    selectedDir = presetApp;
+  } else if (presetApp) {
+    console.log(
+      chalk.red('Unable to find directory with the preset name: ') +
+        chalk.greenBright.bold(presetApp)
+    );
+  }
+
+  while (selectedDir === undefined) {
+    console.log(chalk.bold('Found these directories in parent folder:'));
+    console.log(chalk.bold.blue(dirs.join('\n')));
+    if (failed) {
+      console.log(
+        chalk.red(
+          'Unable to find a directory with that name! Please try again...'
+        )
+      );
+    }
+    const answer = await rl.question(
+      chalk.greenBright(
+        'Write name of project you want to copy local amplify-components into:'
+      ) + `\n> `
+    );
+    selectedDir = dirs.find(
+      (dir) => dir.toLowerCase() === answer.toLowerCase()
+    );
+    if (selectedDir === undefined) {
+      failed = true;
+    }
+  }
+
+  console.clear();
+  console.log(
+    chalk.greenBright(
+      `${chalk.bold.magentaBright(selectedDir)} has been selected.`
+    )
+  );
+
+  await runTask({
+    command: `rm -rf ../${selectedDir}/client/node_modules/.vite`,
+    name: `Removing ${chalk.bold.greenBright(
+      '.vite'
+    )} folder from ${chalk.bold.greenBright(
+      `${selectedDir}/client/node_modules`
+    )}`,
+  });
+
+  await runTask({
+    command: `rm -rf ../${selectedDir}/client/node_modules/.cache`,
+    name: `Removing ${chalk.bold.greenBright(
+      '.cache'
+    )} folder from ${chalk.bold.greenBright(
+      `${selectedDir}/client/node_modules`
+    )}`,
+  });
+
+  await runTask({
+    command: `rm -rf ../${selectedDir}/client/node_modules/@equinor/amplify-components/dist`,
+    name: `Removing old ${chalk.bold.greenBright(
+      'amplify-components/dist'
+    )} folder from ${chalk.bold.greenBright(
+      `${selectedDir}/client/node_modules`
+    )}`,
+  });
+
+  await runTask({
+    command: `cp -r ./dist ../${selectedDir}/client/node_modules/@equinor/amplify-components/dist`,
+    name: `Copying newly built ${chalk.bold.greenBright(
+      'dist'
+    )} folder into ${chalk.bold.greenBright(
+      `${selectedDir}/client/node_modules`
+    )}`,
+  });
+
+  console.log(
+    chalk.green.bold(
+      `Built new version and copied it to ${chalk.bold.magentaBright(
+        selectedDir
+      )}! 🎉`
+    )
+  );
 }
 
-runTasks()
+runTasks();
