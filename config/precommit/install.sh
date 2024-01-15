b6b1ca0e29ba83af40e0dcec8e2e930d746e5909
Marius Tobiassen Bungum <marius@mh2.no>
2024-01-15 09:47:37 +0100

    :sparkles: Added lint-staged as precommit hook instead of custom .mjs node script

--- config/precommit/install.sh
+++ config/precommit/install.sh
@@ -9,25 +9,6 @@ then
   exit 1
 fi
 
-if [ ! -d "tooling" ]
-then
-  mkdir tooling
-fi
-
-cd ./tooling || exit 1
-
-printf -- "Downloading files needed to run pre-commit...\n"
-
-configList=$(curl -s "https://raw.githubusercontent.com/equinor/amplify-components/main/config/precommit/config_list.txt")
-
-for line in $configList
-do
-  fileName=$(echo $line | rev | cut -d '/' -f 1 | rev)
-  curl -s $line > $fileName
-done
-
-cd ..
-
 printf -- "Downloading root package.json file used for pre-commit...\n"
 
 curl -s "https://raw.githubusercontent.com/equinor/amplify-components/main/config/precommit/package.json" > package.json
