c562fb8f2aa09d41e478a03b71d5e321ff5f24e2
Marius Tobiassen Bungum <marius@mh2.no>
2024-01-17 10:03:44 +0100

    :lipstick: Changed code block theme

--- src/components/Inputs/RichTextEditor/RichTextEditor.styles.ts
+++ src/components/Inputs/RichTextEditor/RichTextEditor.styles.ts
@@ -2,9 +2,9 @@ import { tokens } from '@equinor/eds-tokens';
 
 import styled from 'styled-components';
 
-import 'highlight.js/styles/github-dark-dimmed.css';
+import 'highlight.js/styles/base16/solarized-dark.css';
 
-const { colors, typography, spacings } = tokens;
+const { colors, spacings } = tokens;
 
 export const Wrapper = styled.div`
   .tiptap {
@@ -17,20 +17,6 @@ export const Wrapper = styled.div`
       font-size: 16px;
       font-family: 'Equinor', sans-serif;
       color: ${colors.text.static_icons__default.hex};
-    }
-    h1 {
-      font-size: ${typography.heading.h4.fontSize};
-      font-weight: ${typography.heading.h4.fontWeight};
-      line-height: ${typography.heading.h4.lineHeight};
-      color: ${colors.text.static_icons__default.hex};
-      font-family: ${typography.heading.h4.fontFamily};
-    }
-    h2 {
-      font-size: ${typography.heading.h5.fontSize};
-      font-weight: ${typography.heading.h5.fontWeight};
-      line-height: ${typography.heading.h5.lineHeight};
-      color: ${colors.text.static_icons__default.hex};
-      font-family: ${typography.heading.h5.fontFamily};
     }
     img {
       max-width: 100%;
