fcded80a27c4c721550acdfe55c99f5fcd6d5c20
Amanda Elvkull <amanda.elvkull@gmail.com>
2024-02-02 14:02:50 +0100

    :fire: removed old imports

--- src/components/Navigation/TopBar/ApplicationDrawer/ApplicationDrawer.stories.tsx
+++ src/components/Navigation/TopBar/ApplicationDrawer/ApplicationDrawer.stories.tsx
@@ -2,13 +2,10 @@ import { Meta, StoryFn } from '@storybook/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import ApplicationDrawer from './ApplicationDrawer';
-import { decorator } from '../../../../../__mocks__/src/api/services/PortalService';
 
 export default {
   title: 'Navigation/TopBar/ApplicationDrawer',
   component: ApplicationDrawer,
-
-  decorators: [decorator],
 } as Meta;
 
 export const Primary: StoryFn = () => {
