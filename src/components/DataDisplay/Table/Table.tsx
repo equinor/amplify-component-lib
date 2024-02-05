e6c7aa5210bbd8cc8e977907b2a6b16f2188d147
Marius Tobiassen Bungum <marius@mh2.no>
2024-02-05 13:44:54 +0100

    :sparkles: Added Table component (styled EDS Data grid)

---
+++ src/components/DataDisplay/Table/Table.tsx
@@ -0,0 +1,70 @@
+import { EdsDataGrid, EdsDataGridProps } from '@equinor/eds-data-grid-react';
+import { tokens } from '@equinor/eds-tokens';
+
+import { style } from 'src';
+
+import styled from 'styled-components';
+
+const { colors } = tokens;
+const { spacings } = style;
+
+interface WrapperProps {
+  $enableSorting: boolean;
+}
+
+const Wrapper = styled.div<WrapperProps>`
+  table,
+  thead,
+  th {
+    background: transparent;
+  }
+  tr:has(th):hover {
+    background: unset;
+  }
+  thead,
+  th {
+    border: none;
+  }
+  th {
+    color: ${colors.text.static_icons__default.rgba};
+    > div {
+      justify-content: space-between;
+      > svg {
+        height: 18px;
+        width: 18px;
+        fill: ${colors.interactive.primary__resting.rgba};
+      }
+    }
+  }
+  th,
+  td {
+    padding: ${spacings.medium_small} ${spacings.small};
+  }
+  tr:hover,
+  th:hover,
+  th:has(svg) {
+    background: ${colors.interactive.primary__hover_alt.rgba};
+  }
+  ${({ $enableSorting }) =>
+    !$enableSorting &&
+    `
+    cursor: default;
+    tr:hover, th:hover {
+      background: unset;
+      cursor: default;
+    }
+    `}
+`;
+
+function Table<T>(props: EdsDataGridProps<T>) {
+  return (
+    <Wrapper
+      $enableSorting={props.enableSorting || false}
+      data-testid="table-wrapper"
+    >
+      <EdsDataGrid {...props} />
+    </Wrapper>
+  );
+}
+
+export default Table;
