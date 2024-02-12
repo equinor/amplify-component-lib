bd8bf1fb76713128a822083291eab89016a730c3
Brynjulf Risbakken <brynjulf.risbakken@olavstoppen.no>
2024-02-12 15:33:35 +0100

    :fire: remove unnecessary config

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -12,7 +12,7 @@ const commitFile = path.resolve(
 const template = fs.readFileSync(tplFile, "utf-8");
 const commitTemplate = fs.readFileSync(commitFile, "utf-8");
 
-const groupedCommits = require("./config/release/helpers/groupedCommits.cjs");
+const groupedCommits = require("./config/release/helpers/groupedCommits.js");
 
 // Use of Gitmojis to create rules
 const gitmojis = require("gitmojis").gitmojis;
@@ -30,11 +30,6 @@ const RULES = {
     .map(({ emoji }) => emoji),
   patch: gitmojis
     .filter(({ semver }) => semver === PATCH)
-    .map(({ emoji }) => emoji),
-  // Not needed, just if you wish to render commit
-  // with gitmojis without semver.
-  others: gitmojis
-    .filter(({ semver }) => semver === null)
     .map(({ emoji }) => emoji),
 };
 
@@ -61,14 +56,6 @@ module.exports = {
         },
       },
     ],
-    // "@semantic-release/github",
-    // Generate changelog
-    [
-      "@semantic-release/changelog",
-      {
-        changelogFile: "CHANGELOG.md",
-        changelogTitle: "# CHANGELOG",
-      },
-    ],
+    "@semantic-release/github",
   ],
 };
