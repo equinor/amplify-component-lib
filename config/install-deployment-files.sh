249f5b42be7f9fc21cf26351cce8cbb994fc0934
Brynjulf Risbakken <brynjulf.risbakken@olavstoppen.no>
2024-02-13 07:55:31 +0100

    :wrench: added release config files when downloading

--- config/install-deployment-files.sh
+++ config/install-deployment-files.sh
@@ -13,11 +13,19 @@ fi
 
 cd ..
 
-printf -- "Downloading client github actions specific to deployment from github to radix...\n"
-workflowsList=$(curl -s "https://raw.githubusercontent.com/equinor/amplify-components/main/config/github_actions_deployment_list.txt")
+printf -- "Downloading .releaserc.cjs file...\n"
+curl -s "https://raw.githubusercontent.com/equinor/amplify-components/main/config/release/.releaserc.cjs" > .releaserc.cjs
 
-for line in $workflowsList
-do
-  fileName=$(echo $line | rev | cut -d '/' -f 1 | rev)
-  curl -s $line > ".github/workflows/$fileName"
-done
+printf -- "Downloading release config folder...\n"
+cd ./release || (mkdir release && cd ./release || return)
+curl -s "https://raw.githubusercontent.com/equinor/amplify-components/main/config/release/groupMapping.cjs" > groupMapping.cjs
+
+printf -- "Downloading release helper files...\n"
+cd ./helpers || (mkdir helpers && cd ./helpers || return)
+curl -s "https://raw.githubusercontent.com/equinor/amplify-components/main/config/release/helpers/groupedCommits.cjs" > groupedCommits.cjs
+
+cd ..
+printf -- "Downloading release template files...\n"
+cd ./templates || (mkdir templates && cd ./templates || return)
+curl -s "https://raw.githubusercontent.com/equinor/amplify-components/main/config/release/templates/commit-template.hbs" > commit-template.hbs
+curl -s "https://raw.githubusercontent.com/equinor/amplify-components/main/config/release/templates/release-notes-template.hbs" > release-notes-template.hbs
