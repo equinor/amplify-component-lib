bd1b1b7bc54032a0065c7e49c0cb43ed210d83b4
aslakihle <aslak.ihle@gmail.com>
2024-02-23 14:07:02 +0100

    :art: Changed from export default

--- src/components/DataDisplay/Chip/Chip.tsx
+++ src/components/DataDisplay/Chip/Chip.tsx
@@ -2,10 +2,8 @@ import { forwardRef } from 'react';
 
 import { Chip as Base, ChipProps } from '@equinor/eds-core-react';
 
-const Chip = forwardRef<HTMLDivElement, ChipProps>((props, ref) => (
+export const Chip = forwardRef<HTMLDivElement, ChipProps>((props, ref) => (
   <Base ref={ref} {...props} />
 ));
 
 Chip.displayName = 'Chip';
-
-export default Chip;
