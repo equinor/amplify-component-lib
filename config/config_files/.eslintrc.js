1db977a6003b3ec6c983bb7ca494f9b7b5f6b29e
Arkadiy <arst@equinor.com>
2024-02-23 12:56:42 +0100

    copy paste issue

--- config/config_files/.eslintrc.js
+++ config/config_files/.eslintrc.js
@@ -33,7 +33,7 @@ module.exports = {
       'error',
       { props: 'never', children: 'never' },
     ],
-    "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
+    "@typescript-eslint/no-unused-vars": ["error", { "destructuredArrayIgnorePattern": "^_" }],
   },
   overrides: [
     {
