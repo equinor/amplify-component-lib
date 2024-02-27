60a04662cfd929252c47779c8b9ced5b51905ef1
Benjamin Lyon <benjaminlyon0@gmail.com>
2024-02-27 15:45:37 +0100

    add missing props validation

--- src/storybook/PropsTable.tsx
+++ src/storybook/PropsTable.tsx
@@ -1,5 +1,11 @@
 import { ArgsTable, PRIMARY_STORY } from '@storybook/addon-docs';
 
+import PropTypes from 'prop-types';
+
 export const PropsTable = ({ story = PRIMARY_STORY, ...props }) => (
   <ArgsTable story={story} {...props} />
 );
+
+PropsTable.propTypes = {
+  story: PropTypes.string,
+};
