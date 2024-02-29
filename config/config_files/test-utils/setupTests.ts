bf94911afbb3335b96fb0e20d72625396f92c1f2
Brynjulf Risbakken <brynjulf.risbakken@olavstoppen.no>
2024-02-29 10:45:03 +0100

    :white_check_mark: updated setupTests.ts for projects

--- config/config_files/test-utils/setupTests.ts
+++ config/config_files/test-utils/setupTests.ts
@@ -1,15 +1,24 @@
-import { cleanup } from '@testing-library/react';
+import { handlers } from './mock';
 
+import { setupServer } from 'msw/node';
 import { afterEach, beforeAll } from 'vitest';
 
 import '@testing-library/jest-dom';
 
-afterEach(() => {
-  cleanup();
-});
+const server = setupServer(...handlers);
 
 beforeAll(() => {
+  vi.stubEnv('VITE_IS_MOCK', 'true');
+  server.listen({ onUnhandledRequest: 'error' });
   HTMLDialogElement.prototype.show = vi.fn();
   HTMLDialogElement.prototype.showModal = vi.fn();
   HTMLDialogElement.prototype.close = vi.fn();
 });
+
+afterEach(() => {
+  server.resetHandlers();
+});
+
+afterAll(() => server.close());
+
+export { server };
