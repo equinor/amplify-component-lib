f9b6be316f61724aa544c8a5a9dfb61ba1a1921b
Marius Tobiassen Bungum <marius@mh2.no>
2024-03-06 14:43:03 +0100

    :lipstick: Hard-coded paddings for ComboBox since style.spacings don't have the ones we need

--- src/components/Inputs/ComboBox/ComboBox.styles.ts
+++ src/components/Inputs/ComboBox/ComboBox.styles.ts
@@ -17,7 +17,7 @@ export const Container = styled.div<ContainerProps>`
   display: grid;
   grid-template-columns: 1fr auto;
   align-items: center;
-  padding: ${spacings.small} ${spacings.medium_small};
+  padding: 6px 8px;
 
   ${({ $underlineHighlight }) =>
     $underlineHighlight
