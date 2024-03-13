8fb3f7b18486c67d35a27b036ba37e8fb5735410
Birte Jelsa <birte.jelsa@gmail.com>
2024-03-13 16:32:36 +0100

    💄 Align side bar styles to Figma

---
+++ src/components/Navigation/SideBar/MenuItem.styles.ts
@@ -0,0 +1,63 @@
+import { Typography } from '@equinor/eds-core-react';
+import { tokens } from '@equinor/eds-tokens';
+
+import { spacings } from 'src/style';
+
+import styled from 'styled-components';
+
+const { colors } = tokens;
+
+interface LinkProps {
+  $active?: boolean;
+  $open?: boolean;
+  $disabled?: boolean;
+}
+
+export const Link = styled.a<LinkProps>`
+  display: flex;
+  align-self: stretch;
+  align-items: center;
+  justify-content: ${({ $open }) => !$open && 'center'};
+  height: 64px;
+  padding: ${spacings.medium};
+  gap: ${spacings.medium};
+  box-sizing: border-box;
+  border-bottom: 1px solid ${colors.ui.background__medium.rgba};
+  background: ${({ $active }) =>
+    $active && colors.interactive.primary__selected_highlight.rgba};
+  text-decoration: none;
+  transition: background 0.1s ease-out;
+  &:hover {
+    cursor: ${({ $disabled }) => !$disabled && 'pointer'};
+    text-decoration: none;
+    background: ${({ $active, $disabled }) =>
+      !$disabled &&
+      ($active
+        ? colors.interactive.primary__selected_hover.rgba
+        : colors.interactive.primary__hover_alt.rgba)};
+  }
+  &:focus {
+    outline: 1px dashed ${colors.interactive.primary__resting.rgba};
+    text-decoration: none;
+  }
+`;
+
+export const IconContainer = styled.div`
+  padding: ${spacings.x_small};
+  align-items: center;
+  justify-content: center;
+  width: 32px;
+  height: 32px;
+`;
+
+interface ItemTextProps {
+  $active: boolean;
+  $disabled: boolean;
+}
+
+export const ItemText = styled(Typography)<ItemTextProps>`
+  color: ${({ $disabled }) =>
+    $disabled
+      ? colors.interactive.disabled__text.rgba
+      : colors.text.static_icons__default.rgba};
+`;
