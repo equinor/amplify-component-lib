f7c9e882ba0f5e9fea9b6bd83ece34b476e1760b
Birte Jelsa <birte.jelsa@gmail.com>
2024-03-14 14:34:41 +0100

    💄 Align top bar to Figma design

--- src/components/Navigation/TopBar/TopBar.tsx
+++ src/components/Navigation/TopBar/TopBar.tsx
@@ -1,99 +1,19 @@
 import React, { forwardRef, ReactNode } from 'react';
 
-import {
-  CircularProgress as EDSCircularProgress,
-  TopBar as EDSTopBar,
-  Typography,
-} from '@equinor/eds-core-react';
-import { tokens } from '@equinor/eds-tokens';
+import { Typography } from '@equinor/eds-core-react';
 
 import ApplicationIcon from '../../Icons/ApplicationIcon/ApplicationIcon';
+import {
+  AppAndFieldContainer,
+  AppIdentitfier,
+  AppName,
+  Bar,
+  CircularProgress,
+  EnvironmentTag,
+} from './TopBar.styles';
 import FieldSelector from 'src/components/Navigation/TopBar/FieldSelector';
-import { spacings } from 'src/style';
 import { EnvironmentType } from 'src/types/Environment';
 import { Field } from 'src/types/Field';
-
-import styled from 'styled-components';
-
-const { colors } = tokens;
-
-const Bar = styled(EDSTopBar)`
-  border-bottom: 1px solid ${colors.ui.background__medium.rgba};
-  padding-left: ${spacings.medium};
-  position: relative;
-`;
-
-const Header = styled(EDSTopBar.Header)`
-  cursor: pointer;
-  position: relative;
-  display: flex;
-  align-items: center;
-  > header {
-    display: flex;
-    align-items: center;
-    gap: 12px;
-  }
-  > svg {
-    justify-self: center;
-  }
-`;
-
-interface AppNameProps {
-  $capitalize: boolean;
-}
-
-const AppName = styled(Typography)<AppNameProps>`
-  text-transform: ${(props) => (props.$capitalize ? 'capitalize' : 'none')};
-`;
-
-const CircularProgress = styled(EDSCircularProgress)`
-  position: absolute;
-  right: -24px;
-
-  circle {
-    stroke: ${colors.interactive.primary__resting.rgba};
-  }
-  circle:first-child {
-    stroke: ${colors.interactive.primary__hover_alt.rgba};
-  }
-`;
-
-interface EnvironmentTagProps {
-  $environmentType: EnvironmentType;
-}
-
-function environmentStyling(envType: EnvironmentType): string {
-  let backgroundColor = 'none';
-  let borderColor = 'none';
-  if (envType === EnvironmentType.LOCALHOST) {
-    backgroundColor = colors.interactive.disabled__fill.rgba;
-    borderColor = colors.interactive.disabled__text.rgba;
-  } else if (envType === EnvironmentType.DEVELOP) {
-    backgroundColor = colors.ui.background__info.rgba;
-    borderColor = colors.infographic.substitute__blue_overcast.rgba;
-  } else if (envType === EnvironmentType.STAGING) {
-    backgroundColor = colors.ui.background__warning.rgba;
-    borderColor = colors.interactive.warning__text.rgba;
-  }
-  return `
-    background-color: ${backgroundColor};
-    border: 1px solid ${borderColor};
-  `;
-}
-
-const EnvironmentTag = styled.div<EnvironmentTagProps>`
-  width: 214px;
-  height: 36px;
-  border-radius: 4px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: absolute;
-  left: 50%;
-  transform: translateX(-50%);
-  text-transform: capitalize;
-  ${(props) => environmentStyling(props.$environmentType)}
-`;
 
 type TopBarType = {
   onHeaderClick: () => void;
@@ -128,13 +48,17 @@ export const TopBar = forwardRef<HTMLDivElement, TopBarType>(
   ) => {
     return (
       <Bar ref={ref}>
-        <Header>
-          <header onClick={onHeaderClick}>
+        <AppAndFieldContainer>
+          <AppIdentitfier onClick={onHeaderClick} tabIndex={0}>
             <ApplicationIcon name={applicationIcon} size={32} withHover />
-            <AppName variant="h6" $capitalize={capitalize}>
+            <AppName
+              group="navigation"
+              variant="menu_title"
+              $capitalize={capitalize}
+            >
               {capitalize ? applicationName.toLowerCase() : applicationName}
             </AppName>
-          </header>
+          </AppIdentitfier>
           {availableFields && onSelectField && (
             <FieldSelector
               availableFields={availableFields}
@@ -144,7 +68,7 @@ export const TopBar = forwardRef<HTMLDivElement, TopBarType>(
             />
           )}
           {isFetching && <CircularProgress size={16} />}
-        </Header>
+        </AppAndFieldContainer>
         {(environment === EnvironmentType.DEVELOP ||
           environment === EnvironmentType.STAGING ||
           environment === EnvironmentType.LOCALHOST) && (
