f9aaf5839750119e9d95c5268494cde42e910ef0
Benjamin Lyon <benjaminlyon0@gmail.com>
2024-04-04 15:05:20 +0200

    🔧 add env variable to env.sh

--- config/config_files/env.sh
+++ config/config_files/env.sh
@@ -24,6 +24,9 @@ echo "  ENVIRONMENT_NAME: \"$value\"," >>./env-config.js
 value=$(printf '%s\n' "${SERVICE_NOW_CONFIGURATION_ITEM}")
 echo "  SERVICE_NOW_CONFIGURATION_ITEM: \"$value\"," >>./env-config.js
 
+value=$(printf '%s\n' "${API_SUBSCRIPTION_KEY}")
+echo "  API_SUBSCRIPTION_KEY: \"$value\"," >>./env-config.js
+
 echo "}" >>./env-config.js
 
 # Update nginx.conf and securityheaders.conf with relevant environment variables
