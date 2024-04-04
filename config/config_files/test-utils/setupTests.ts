167cf83b60cace451bdc616fbd70602d79b058e3
Birte <birte.jelsa@gmail.com>
2024-04-04 08:33:29 +0200

    🔧 Update test config (#522)

--- config/config_files/test-utils/setupTests.ts
+++ config/config_files/test-utils/setupTests.ts
@@ -1,5 +1,8 @@
+import { IconData } from '@equinor/eds-icons';
+import { cleanup, within } from '@testing-library/react';
 import { handlers as appHandlers } from './mock';
 import { handlers as portalHandlers } from './portalMock';
+import { MatcherResult } from './vitest';
 
 import { setupServer } from 'msw/node';
 import { afterEach, beforeAll } from 'vitest';
@@ -14,9 +17,29 @@ beforeAll(() => {
   HTMLDialogElement.prototype.show = vi.fn();
   HTMLDialogElement.prototype.showModal = vi.fn();
   HTMLDialogElement.prototype.close = vi.fn();
+
+  expect.extend({
+    toContainEDSIcon(received: HTMLElement, expected: IconData): MatcherResult {
+      const svgPaths = within(received).getAllByTestId('eds-icon-path');
+      const pass = !!svgPaths.find(
+        (p) => p.getAttribute('d') === expected.svgPathData
+      );
+      const message = () =>
+        `Received element ${
+          pass
+            ? 'contains an EDS icon with name:'
+            : 'does not contain an EDS icon with name:'
+        } ${expected.name}`;
+      return {
+        pass,
+        message,
+      };
+    },
+  });
 });
 
 afterEach(() => {
+  cleanup();
   server.resetHandlers();
 });
 
