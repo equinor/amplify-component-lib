a6248f2d17516a9922b95bab3896b41a4f43fe10
Arkadiy Stepanov <arst@equinor.com>
2024-04-05 12:58:50 +0200

    🚧 Add tests

--- src/components/DataDisplay/Chip/Chip.styles.tsx
+++ src/components/DataDisplay/Chip/Chip.styles.tsx
@@ -10,21 +10,19 @@ interface ChipProps {
 }
 
 export const StyledChip = styled(Base)<ChipProps>`
-  && {
-    border: 1px solid ${colors.ui.background__medium.rgba};
-    ${({ $readonly }) =>
-      $readonly
-        ? css`
-            color: ${colors.text.static_icons__default.rgba};
-          `
-        : css`
-            color: ${colors.infographic.primary__moss_green_100.rgba};
+  border: 1px solid ${colors.ui.background__medium.rgba};
+  ${({ $readonly }) =>
+    $readonly
+      ? css`
+          color: ${colors.text.static_icons__default.rgba} !important;
+        `
+      : css`
+          color: ${colors.infographic.primary__moss_green_100.rgba} !important;
 
-            &:hover {
-              background: ${colors.interactive.primary__hover_alt.rgba};
-              color: ${colors.interactive.primary__hover.rgba};
-              border: 1px solid ${colors.interactive.primary__hover.rgba};
-            }
-          `};
-  }
+          &:hover {
+            background: ${colors.interactive.primary__hover_alt.rgba};
+            color: ${colors.interactive.primary__hover.rgba} !important;
+            border: 1px solid ${colors.interactive.primary__hover.rgba};
+          }
+        `};
 `;
--- src/components/DataDisplay/Chip/Chip.test.tsx
+++ src/components/DataDisplay/Chip/Chip.test.tsx
@@ -1,4 +1,7 @@
+import { tokens } from '@equinor/eds-tokens';
 import { faker } from '@faker-js/faker';
+
+const { colors } = tokens;
 
 import { Chip } from './Chip';
 import { render, screen } from 'src/tests/test-utils';
@@ -6,5 +9,34 @@ import { render, screen } from 'src/tests/test-utils';
 test('Shows chip', () => {
   const someText = faker.animal.crocodilia();
   render(<Chip>{someText}</Chip>);
+
   expect(screen.getByText(someText)).toBeInTheDocument();
+  expect(screen.getByText(someText)).toHaveStyleRule(
+    'color',
+    `${colors.infographic.primary__moss_green_100.rgba}!important`
+  );
+
+  const hoverOption = { modifier: ':hover' };
+
+  expect(screen.getByText(someText)).toHaveStyleRule(
+    'color',
+    `${colors.interactive.primary__hover.rgba}!important`,
+    hoverOption
+  );
+
+  expect(screen.getByText(someText)).toHaveStyleRule(
+    'background',
+    `${colors.interactive.primary__hover_alt.rgba}`,
+    hoverOption
+  );
+});
+
+test('Shows readonly chip', () => {
+  const someText = faker.animal.crocodilia();
+  render(<Chip readonly>{someText}</Chip>);
+
+  expect(screen.queryByText(someText)).toHaveStyleRule(
+    'color',
+    `${colors.text.static_icons__default.rgba}!important`
+  );
 });
