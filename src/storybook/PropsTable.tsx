05058c832e44732e9e694eb16b6ccbc073463f10
Marius Tobiassen Bungum <marius@mh2.no>
2024-04-15 09:02:49 +0200

    :arrow_up: Swapped to using 'ArgTypes' instead of 'ArgsTable'

--- src/storybook/PropsTable.tsx
+++ src/storybook/PropsTable.tsx
@@ -1,9 +1,9 @@
-import { ArgsTable, PRIMARY_STORY } from '@storybook/addon-docs';
+import { ArgTypes, PRIMARY_STORY } from '@storybook/addon-docs';
 
 import PropTypes from 'prop-types';
 
 export const PropsTable = ({ story = PRIMARY_STORY, ...props }) => (
-  <ArgsTable story={story} {...props} />
+  <ArgTypes of={story} {...props} />
 );
 
 PropsTable.propTypes = {
