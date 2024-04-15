557819daf5cbf2b3480be32264446bd91ccb2c95
Benjamin Lyon <benjaminlyon0@gmail.com>
2024-04-15 14:42:06 +0200

    ✨ Add timeout to playwright actions
    In the case that a `click()` is not able to resolve because the object it is clicking it undefined, we want to timeout the test.

--- config/config_files/playwright.config.ts
+++ config/config_files/playwright.config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
-    actionTimeout: 0,
+    actionTimeout: 10000,
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: `http://localhost:${PORT}`,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
