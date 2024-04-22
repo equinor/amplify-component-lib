4c66b49ab264d44236e93c7a1d4477d43b1a3b90
Arkadiy Stepanov <arst@equinor.com>
2024-04-22 11:08:59 +0200

    🚧 Revert commit

--- src/components/Navigation/TableOfContents/TableOfContents.styles.ts
+++ src/components/Navigation/TableOfContents/TableOfContents.styles.ts
@@ -35,7 +35,7 @@ export const Button = styled.button<ButtonProps>`
     text-align: left;
     font-family: 'Equinor', sans-serif;
     font-weight: 700;
-    font-size: ${({ $active }) => ($active ? 16 : 14)}px;
+    font-size: 14px;
   }
   ${({ $variant, $active }) => {
     switch ($variant) {
