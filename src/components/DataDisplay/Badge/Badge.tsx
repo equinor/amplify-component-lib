7fc5acec9b87d2a8e2ca1bfa1808aafcd47655ac
Arkadiy Stepanov <arst@equinor.com>
2024-04-25 10:19:57 +0200

    ✨ Create and use Badge component

---
+++ src/components/DataDisplay/Badge/Badge.tsx
@@ -0,0 +1,22 @@
+import { FC } from 'react';
+
+import { Typography } from '@equinor/eds-core-react';
+
+import { Container } from './Badge.styles';
+
+export type Variants = 'default' | 'light' | 'danger' | 'empty';
+
+export interface BadgeProps {
+  value: number;
+  variant?: Variants;
+}
+
+export const Badge: FC<BadgeProps> = ({ value, variant = 'default' }) => {
+  return (
+    <Container data-testid="badge-container" $variant={variant}>
+      <Typography as="span" variant="label" group="navigation">
+        {value}
+      </Typography>
+    </Container>
+  );
+};
