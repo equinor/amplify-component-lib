fb58ca634c18aaa9ef600f30164e8ea933da2d29
aslakihle <aslak.ihle@gmail.com>
2024-05-15 12:26:41 +0200

    :sparkles: Text prop and optional URL on MissingAccesses on ErrorPage

--- src/components/Feedback/ErrorPage/MissingAccesses.tsx
+++ src/components/Feedback/ErrorPage/MissingAccesses.tsx
@@ -16,38 +16,47 @@ const Container = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: center;
+    gap: ${spacings.medium};
   }
 `;
 
-interface AccessType { url: string; title: string }
+interface AccessType {
+  url?: string;
+  title: string;
+}
 interface MissingAccessesProps {
   accesses?: AccessType[];
+  text?: string;
 }
 export const MissingAccesses = forwardRef<HTMLDivElement, MissingAccessesProps>(
-  ({ accesses = [] }, ref) => (
-    <Container ref={ref}>
-      <Typography group="paragraph" variant="body_long_bold">
-        You can apply for access to the app in AccessIT:
-      </Typography>
-      {accesses.map((access, index) => (
-        <div key={index}>
-          <Typography group="paragraph" variant="body_short">
-            {access.title}
-          </Typography>
-          <Button
-            data-testid={`missing-access-button-${access.title}`}
-            variant="ghost_icon"
-            onClick={() => window.open(access.url, '_blank')}
-          >
-            <Icon
-              data={external_link}
-              color={colors.text.static_icons__default.rgba}
-            />
-          </Button>
-        </div>
-      ))}
-    </Container>
-  )
+  ({ accesses = [], text }, ref) => {
+    return (
+      <Container ref={ref}>
+        <Typography group="paragraph" variant="body_long_bold">
+          {text ?? 'You can apply for access to the app in AccessIT:'}
+        </Typography>
+        {accesses?.map((access, index) => (
+          <div key={index}>
+            <Typography group="paragraph" variant="body_short">
+              {access.title}
+            </Typography>
+            {access.url && (
+              <Button
+                data-testid={`missing-access-button-${access.title}`}
+                variant="ghost_icon"
+                onClick={() => window.open(access.url, '_blank')}
+              >
+                <Icon
+                  data={external_link}
+                  color={colors.text.static_icons__default.rgba}
+                />
+              </Button>
+            )}
+          </div>
+        ))}
+      </Container>
+    );
+  }
 );
 
 MissingAccesses.displayName = 'ErrorPage.MissingAccesses';
