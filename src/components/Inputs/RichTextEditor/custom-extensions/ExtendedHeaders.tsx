1ef042d57080cde4c663e7713bbebff35af6f565
Samuel M. Bednarz <samuelmorkbednarz@gmail.com>
2024-07-03 14:44:51 +0200

    Finish EDS typography header alignment

--- src/components/Inputs/RichTextEditor/custom-extensions/ExtendedHeaders.tsx
+++ src/components/Inputs/RichTextEditor/custom-extensions/ExtendedHeaders.tsx
@@ -1,17 +1,23 @@
-import { useState } from 'react';
-import { mergeAttributes, Node } from '@tiptap/core';
 import {
   ReactNodeViewRenderer,
   NodeViewContent,
   NodeViewWrapper,
 } from '@tiptap/react';
-import { Button, Divider, Icon, Typography } from '../../../';
+import { Typography } from '../../../';
+import { TypographyVariants } from '@equinor/eds-core-react/dist/types/components/Typography/Typography.tokens';
 import Heading from '@tiptap/extension-heading';
 
-const Component = (props) => {
+const Component = (props: any) => {
+  const level = props.node.attrs.level;
+  let variant: TypographyVariants = 'h1';
+  if (level === 2) variant = 'h2';
+  if (level === 3) variant = 'h3';
+  if (level === 4) variant = 'h4';
+  if (level === 5) variant = 'h5';
+  if (level === 6) variant = 'h6';
   return (
     <NodeViewWrapper>
-      <Typography variant="h1">
+      <Typography variant={variant}>
         <NodeViewContent className="content is-editable" />
       </Typography>
     </NodeViewWrapper>
