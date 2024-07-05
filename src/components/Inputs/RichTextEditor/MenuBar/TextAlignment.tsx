39ac8bd4362fe25aab81596228d3087ac6d72c54
Samuel M. Bednarz <samuelmorkbednarz@gmail.com>
2024-07-05 12:48:56 +0200

    more typos

--- src/components/Inputs/RichTextEditor/MenuBar/TextAlignment.tsx
+++ src/components/Inputs/RichTextEditor/MenuBar/TextAlignment.tsx
@@ -10,7 +10,7 @@ import { EditorPanel, RichTextEditorFeatures } from '../RichTextEditor.types';
 import { EditorMenu } from './MenuBar';
 
 const TextAlignment: FC<EditorPanel> = ({ editor, features }) => {
-  /* c8 ignore start */ // Testing tese lines would just be testing the tiptap library or testing that JavaScript works. Theres not enough custom logic here to warrant the maintance cost
+  /* c8 ignore start */ // Testing these lines would just be testing the tiptap library or testing that JavaScript works. Theres not enough custom logic here to warrant the maintance cost
   const alignLeft = () => editor.chain().focus().setTextAlign('left').run();
   const alignCenter = () => editor.chain().focus().setTextAlign('center').run();
   const alignRight = () => editor.chain().focus().setTextAlign('right').run();
--- src/components/Inputs/RichTextEditor/MenuBar/TextCode.tsx
+++ src/components/Inputs/RichTextEditor/MenuBar/TextCode.tsx
@@ -6,7 +6,7 @@ import { EditorPanel, RichTextEditorFeatures } from '../RichTextEditor.types';
 import { EditorMenu } from './MenuBar';
 
 const TextCode: FC<EditorPanel> = ({ editor, features }) => {
-  /* c8 ignore start */ // Testing tese lines would just be testing the tiptap library or testing that JavaScript works. Theres not enough custom logic here to warrant the maintance cost
+  /* c8 ignore start */ // Testing these lines would just be testing the tiptap library or testing that JavaScript works. Theres not enough custom logic here to warrant the maintance cost
   const toggleCode = () => editor.chain().focus().toggleCodeBlock().run();
   if (features && !features.includes(RichTextEditorFeatures.CODE)) return;
   /* c8 ignore end */
