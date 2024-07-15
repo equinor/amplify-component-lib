3714c1bfea0743c656fdeaefc230d9e0a1e50197
aslakihle <aslak.ihle@gmail.com>
2024-07-15 10:13:04 +0200

    :white_check_mark: Coverage fix

--- src/tests/test-utils.ts
+++ src/tests/test-utils.ts
@@ -1,5 +1,6 @@
 import { ReactElement } from 'react';
 
+import { faker } from '@faker-js/faker';
 import { render, RenderOptions } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
@@ -7,6 +8,17 @@ const customRender = (
   ui: ReactElement,
   options?: RenderOptions
 ): ReturnType<typeof render> => render(ui, options);
+
+export function fakeSelectItem() {
+  return {
+    label: faker.string.uuid(),
+    value: faker.string.uuid(),
+  };
+}
+
+export function fakeSelectItems(count = 10) {
+  return new Array(count).fill(0).map(() => fakeSelectItem());
+}
 
 // re-export everything
 export * from '@testing-library/react';
