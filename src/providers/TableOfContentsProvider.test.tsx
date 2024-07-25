85801812087aab7a95c3de8e06cc3c2efb30a59b
Marius Tobiassen Bungum <marius@mh2.no>
2024-07-25 13:57:42 +0200

    :white_check_mark: Added delay to TableOfContentsProvider test

--- src/providers/TableOfContentsProvider.test.tsx
+++ src/providers/TableOfContentsProvider.test.tsx
@@ -142,46 +142,50 @@ test('Manual scroll settings work as expected and do not affect menu scroll', as
   });
 });
 
-test('Should scroll instantly to element when shouldInstantlyJumpOnMount and a hash is present is set and then', async () => {
-  const items = fakeItems();
+test(
+  'Should scroll instantly to element when shouldInstantlyJumpOnMount and a hash is present is set and then',
+  async () => {
+    const items = fakeItems();
 
-  render(
-    <div>
-      <TableOfContents />
-      <TestContainer items={items} shouldInstantlyJumpOnMount />
-    </div>,
-    {
-      wrapper: (props: { children: ReactNode }) => (
-        <MemoryRouter initialEntries={['/#1']}>
-          <TableOfContentsProvider items={items}>
-            {props.children}
-          </TableOfContentsProvider>
-        </MemoryRouter>
-      ),
-    }
-  );
+    render(
+      <div>
+        <TableOfContents />
+        <TestContainer items={items} shouldInstantlyJumpOnMount />
+      </div>,
+      {
+        wrapper: (props: { children: ReactNode }) => (
+          <MemoryRouter initialEntries={['/#1']}>
+            <TableOfContentsProvider items={items}>
+              {props.children}
+            </TableOfContentsProvider>
+          </MemoryRouter>
+        ),
+      }
+    );
 
-  const user = userEvent.setup();
+    const user = userEvent.setup({ delay: 1000 });
 
-  const header = document.querySelector(`#${items[1].value}`)!;
-  const headerSpy = vi.spyOn(header, 'scrollIntoView');
+    const header = document.querySelector(`#${items[1].value}`)!;
+    const headerSpy = vi.spyOn(header, 'scrollIntoView');
 
-  await user.click(header);
+    await user.click(header);
 
-  expect(headerSpy).toHaveBeenCalledWith({
-    block: 'start',
-    behavior: 'instant',
-  });
+    expect(headerSpy).toHaveBeenCalledWith({
+      block: 'start',
+      behavior: 'instant',
+    });
 
-  // Move to other
-  const otherButton = screen.getByRole('button', { name: items[7].label });
-  const otherHeader = document.querySelector(`#${items[7].value}`)!;
-  const otherHeaderSpy = vi.spyOn(otherHeader, 'scrollIntoView');
+    // Move to other
+    const otherButton = screen.getByRole('button', { name: items[7].label });
+    const otherHeader = document.querySelector(`#${items[7].value}`)!;
+    const otherHeaderSpy = vi.spyOn(otherHeader, 'scrollIntoView');
 
-  await user.click(otherButton);
+    await user.click(otherButton);
 
-  expect(otherHeaderSpy).toHaveBeenCalledWith({
-    block: 'start',
-    behavior: 'smooth',
-  });
-});
+    expect(otherHeaderSpy).toHaveBeenCalledWith({
+      block: 'start',
+      behavior: 'smooth',
+    });
+  },
+  { timeout: 8000 }
+);
