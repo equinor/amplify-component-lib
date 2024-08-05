85b96df4102e9f9539c17365b9edf4a7507e1ca8
Marius Tobiassen Bungum <marius@mh2.no>
2024-08-05 14:05:26 +0200

    :sparkles: Added new formatDataSize util fn

--- src/atoms/utils/size.test.ts
+++ src/atoms/utils/size.test.ts
@@ -1,16 +1,18 @@
+import { faker } from '@faker-js/faker';
+
 import size from './size';
 
-const tests = [
-  { value: 1, expected: '1 Byte' },
-  { value: 1024, expected: '1 KB' },
-  { value: 1024 * 10, expected: '10 KB' },
-  { value: 1024 * 100, expected: '100 KB' },
-  { value: Math.pow(1024, 2), expected: '1 MB' },
-  { value: Math.pow(1024, 3), expected: '1 GB' },
-];
-
 test('testing multiple test cases', () => {
-  tests.forEach(({ value, expected }) => {
+  const test_cases = [
+    { value: 1, expected: '1 Byte' },
+    { value: 1024, expected: '1 KB' },
+    { value: 1024 * 10, expected: '10 KB' },
+    { value: 1024 * 100, expected: '100 KB' },
+    { value: Math.pow(1024, 2), expected: '1 MB' },
+    { value: Math.pow(1024, 3), expected: '1 GB' },
+  ];
+
+  test_cases.forEach(({ value, expected }) => {
     expect(size.formatBytes(value)).toBe(expected);
   });
 });
@@ -35,4 +37,48 @@ test('testing with negative decimal', () => {
 
 test('testing kiloBytes', () => {
   expect(size.formatKiloBytes(Math.pow(1024, 2))).toBe('1 GB');
+});
+
+test('formatDataSize works as expected with default inputFormat', () => {
+  const fakeSize = faker.number.int({ min: 10, max: 1023 });
+
+  const parsed = size.formatDataSize({ size: fakeSize });
+
+  expect(parsed).toMatch(`${fakeSize} KiB`);
+});
+
+test('formatDataSize works as expected with inputFormat=B', () => {
+  const fakeSize = faker.number.int({ min: 10, max: 1023 });
+
+  const parsed = size.formatDataSize({ size: fakeSize, inputFormat: 'B' });
+
+  expect(parsed).toMatch(`${fakeSize} B`);
+});
+
+test('formatDataSize works as expected with number between 1024 < 1024^2', () => {
+  const fakeSize = faker.number.int({ min: 1024, max: 1024 * 1023 });
+
+  const parsed = size.formatDataSize({ size: fakeSize });
+
+  expect(parsed).toMatch(/MiB/);
+});
+
+test('formatDataSize works as expected with 0', () => {
+  const parsed = size.formatDataSize({ size: 0 });
+
+  expect(parsed).toMatch('0 KiB');
+});
+
+test('formatDataSize throws error if providing invalid size', () => {
+  const fakeSize = faker.number.int({ min: -1024, max: -1 });
+
+  expect(() => size.formatDataSize({ size: fakeSize })).toThrowError();
+});
+
+test('formatDataSize throws error if providing invalid decimals', () => {
+  const fakeSize = faker.number.int({ min: 1024, max: 1024 * 1024 });
+
+  expect(() =>
+    size.formatDataSize({ size: fakeSize, decimals: -1 })
+  ).toThrowError();
 });
--- src/atoms/utils/size.ts
+++ src/atoms/utils/size.ts
@@ -1,5 +1,8 @@
 const k = 1024; // using decimal to give a more realistic value from blobStorage
 
+/**
+ * @deprecated - Use new 'formatDataSize' instead
+ */
 const formatBytes = (bytes: number, decimals = 2): string => {
   if (bytes < 0) return '0 Bytes';
   if (bytes === 0) return '0 Bytes';
@@ -13,8 +16,71 @@ const formatBytes = (bytes: number, decimals = 2): string => {
   return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
 };
 
+/**
+ * @deprecated - Use new 'formatDataSize' instead
+ */
 const formatKiloBytes = (kiloBytes: number, decimals = 2): string => {
   return formatBytes(kiloBytes * k, decimals);
 };
 
-export default { formatBytes, formatKiloBytes };
+type InputFormats =
+  | 'B'
+  | 'KiB'
+  | 'MiB'
+  | 'GiB'
+  | 'TiB'
+  | 'PiB'
+  | 'EiB'
+  | 'ZiB'
+  | 'YiB';
+
+interface FormatDataSizeParams {
+  size: number;
+  decimals?: number;
+  inputFormat?: InputFormats;
+}
+
+const SIZES: InputFormats[] = [
+  'B',
+  'KiB',
+  'MiB',
+  'GiB',
+  'TiB',
+  'PiB',
+  'EiB',
+  'ZiB',
+  'YiB',
+];
+
+/**
+ * @param size - Size in whatever the input format is set to
+ * @param decimals - Amount of decimals to round to, defaults to 2
+ * @param inputFormat - The format that size is in, defaults to 'KiB'
+ * @returns - Formatted data size to human readable, i.e. '1024 KiB' = '1 MiB'
+ */
+function formatDataSize({
+  size,
+  decimals = 2,
+  inputFormat = 'KiB',
+}: FormatDataSizeParams): string {
+  if (size < 0) {
+    throw new Error('Invalid size, must be a positive integer');
+  }
+
+  if (decimals < 0) {
+    throw new Error('Invalid decimals, must be a positive integer');
+  }
+
+  if (size === 0) return `${size} ${inputFormat}`;
+
+  const inputFormatIndex = SIZES.indexOf(inputFormat);
+  const multipliedSize = size * Math.pow(1024, inputFormatIndex);
+
+  const index = Math.floor(Math.log(multipliedSize) / Math.log(1024));
+  const parsedSize = parseFloat(
+    (multipliedSize / Math.pow(1024, index)).toFixed(decimals)
+  );
+  return `${parsedSize} ${SIZES[index]}`;
+}
+
+export default { formatBytes, formatKiloBytes, formatDataSize };
