4cff69a6322d4581bf5da087c0cb58efdbe34a45
Marius Tobiassen Bungum <marius@mh2.no>
2024-08-06 14:32:28 +0200

    :recycle: Refactored InfoElement

--- src/molecules/InfoElement/InfoElement.tsx
+++ src/molecules/InfoElement/InfoElement.tsx
@@ -5,37 +5,50 @@ import CopyText from 'src/molecules/InfoElement/CopyText';
 
 export interface InfoElementProps {
   title: string;
-  content: ReactElement | string;
-  capitalizeContent?: boolean;
-  /* @deprecated This prop will be removed when CopyText gets removed */
-  copyableContent?: boolean;
 }
 
-export const InfoElement = forwardRef<HTMLDivElement, InfoElementProps>(
-  ({ title, content, copyableContent, capitalizeContent }, ref) => {
-    const contentElement =
-      capitalizeContent && typeof content === 'string'
-        ? content.toUpperCase()
-        : content;
-    return (
-      <div ref={ref}>
-        <Typography group="paragraph" variant="overline">
-          {title?.toUpperCase()}
-        </Typography>
-        {typeof content === 'string' ? (
-          copyableContent ? (
-            <CopyText textToCopy={content}>
-              <Typography variant="h6">{contentElement}</Typography>
-            </CopyText>
-          ) : (
-            <Typography variant="h6">{contentElement}</Typography>
-          )
-        ) : (
-          content
-        )}
-      </div>
+/**
+ * @param content - Text
+ * @param copyableContent - Should the content be copyable via hover - Defaults to false
+ * @param capitalizeContent - Should the content be capitalized - Defaults to false
+ */
+interface TextInfoElementProps extends InfoElementProps {
+  content: string;
+  copyableContent?: true;
+  capitalizeContent?: true;
+}
+
+interface CustomInfoElementProps extends InfoElementProps {
+  content: ReactElement;
+  copyableContent?: undefined;
+  capitalizeContent?: undefined;
+}
+
+export const InfoElement = forwardRef<
+  HTMLDivElement,
+  TextInfoElementProps | CustomInfoElementProps
+>(({ title, content, copyableContent, capitalizeContent }, ref) => {
+  const contentElement =
+    typeof content === 'string' ? (
+      <Typography variant="h6">
+        {capitalizeContent ? content.toUpperCase() : content}
+      </Typography>
+    ) : (
+      content
     );
-  }
-);
+
+  return (
+    <div ref={ref}>
+      <Typography group="paragraph" variant="overline">
+        {title?.toUpperCase()}
+      </Typography>
+      {copyableContent ? (
+        <CopyText textToCopy={content}>{contentElement}</CopyText>
+      ) : (
+        contentElement
+      )}
+    </div>
+  );
+});
 
 InfoElement.displayName = 'InfoElement';
