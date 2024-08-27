26e7589a1eb4f936771ea9541ab264bc947a8c40
Marius Tobiassen Bungum <marius@mh2.no>
2024-08-27 14:15:16 +0200

    :sparkles: Added ThemeProvider

---
+++ src/providers/ThemeProvider/ThemeProvider.test.tsx
@@ -0,0 +1,37 @@
+import { act, FC, ReactNode } from 'react';
+
+import { ThemeProvider, useThemeProvider } from './ThemeProvider';
+import { THEME_LOCALSTORAGE_KEY } from './ThemeProvider.constants';
+import { Theme } from 'src/atoms/enums/Theme';
+import { renderHook } from 'src/tests/test-utils';
+
+const Wrapper: FC<{ children: ReactNode }> = ({ children }) => {
+  return (
+    <ThemeProvider>
+      {children}
+      <div>text</div>
+    </ThemeProvider>
+  );
+};
+
+test('Sets data-theme as expected', () => {
+  const { result } = renderHook(() => useThemeProvider(), {
+    wrapper: Wrapper,
+  });
+
+  act(() => result.current.setTheme(Theme.DARK));
+
+  expect(document.documentElement).toHaveAttribute('data-theme', Theme.DARK);
+});
+
+test('Initializes correctly', () => {
+  window.localStorage.setItem(
+    THEME_LOCALSTORAGE_KEY,
+    JSON.stringify(Theme.DARK)
+  );
+  renderHook(() => useThemeProvider(), {
+    wrapper: Wrapper,
+  });
+
+  expect(document.documentElement).toHaveAttribute('data-theme', Theme.DARK);
+});
---
+++ src/providers/ThemeProvider/ThemeProvider.tsx
@@ -0,0 +1,49 @@
+import { createContext, FC, ReactNode, useContext, useEffect } from 'react';
+
+import { THEME_LOCALSTORAGE_KEY } from './ThemeProvider.constants';
+import { useLocalStorage } from 'src/atoms';
+import { Theme } from 'src/atoms/enums/Theme';
+
+interface ThemeProviderContextType {
+  theme: Theme;
+  setTheme: (theme: Theme) => void;
+}
+
+const ThemeProviderContext = createContext<
+  ThemeProviderContextType | undefined
+>(undefined);
+
+export function useThemeProvider() {
+  const context = useContext(ThemeProviderContext);
+  if (context === undefined) {
+    throw new Error('ThemeProvider must be used within Provider');
+  }
+  return context;
+}
+
+interface ThemeProviderProps {
+  children: ReactNode;
+}
+
+export const ThemeProvider: FC<ThemeProviderProps> = ({ children }) => {
+  const [theme, setTheme] = useLocalStorage<Theme>(
+    THEME_LOCALSTORAGE_KEY,
+    Theme.LIGHT
+  );
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+  }, [theme]);
+
+  const handleOnSetTheme = (value: Theme) => {
+    setTheme(value);
+  };
+
+  return (
+    <ThemeProviderContext.Provider
+      value={{ theme, setTheme: handleOnSetTheme }}
+    >
+      {children}
+    </ThemeProviderContext.Provider>
+  );
+};
