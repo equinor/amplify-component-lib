0088897fbe544b2d257fbcc7d5271004d8b8e987
Marius Tobiassen Bungum <marius@mh2.no>
2024-09-26 11:14:09 +0200

    :bug: Fix looping over file not working as expected in generate script

--- config/config_files/check_config_workflow/generate_check_config.sh
+++ config/config_files/check_config_workflow/generate_check_config.sh
@@ -1,12 +1,12 @@
 # Setup base
-curl -s "https://raw.githubusercontent.com/equinor/amplify-component-lib/main/config/config_files/check_config_workflow/base.yaml" > ".github/workflows/check_config.yaml"
+curl -s "https://raw.githubusercontent.com/equinor/amplify-component-lib/main/config/config_files/check_config_workflow/base.yaml" > "test"
 
 # Loop through the list from check_config/list.txt
 list=$(curl -s "https://raw.githubusercontent.com/equinor/amplify-component-lib/main/config/config_files/check_config_workflow/list.txt")
 
-for line in $list
+while read line;
 do
-  read -r var1 var2 var3 <<< "$line"
+  read var1 var2 var3 <<< "$line"
   printf -- "Checking $var1, $var2"
   if grep -q $var1 "./client/.acl-ignore"; then
     printf -- "$var1 in .acl-ignore, skipping...\n"
@@ -16,6 +16,6 @@ do
         working-directory: $var2
         run: diff $var1 <(curl $var3)
     "
-    echo "$newLine" >> ".github/workflows/check_config.yaml"
+    echo "$newLine" >> "test"
   fi
-done
+done < <(echo "$list")
