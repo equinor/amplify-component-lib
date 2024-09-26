81984346b34088eb7ba5f1a5774b5e7a204a475b
Marius Tobiassen Bungum <marius@mh2.no>
2024-09-26 12:16:44 +0200

    :recycle: Simplified isCurrentUrl

--- src/organisms/SideBar/MenuItem.tsx
+++ src/organisms/SideBar/MenuItem.tsx
@@ -3,7 +3,7 @@ import { forwardRef, HTMLAttributes, MouseEvent, useMemo } from 'react';
 import { Icon } from '@equinor/eds-core-react';
 
 import { IconContainer, ItemText, Link } from './MenuItem.styles';
-import { isCurrentUrl as checkUrl } from './MenuItem.utils';
+import { isCurrentUrl } from './MenuItem.utils';
 import { SideBarMenuItem } from 'src/atoms/types/SideBar';
 import { OptionalTooltip } from 'src/molecules/OptionalTooltip/OptionalTooltip';
 import { useSideBar } from 'src/providers/SideBarProvider';
@@ -11,12 +11,6 @@ import { useSideBar } from 'src/providers/SideBarProvider';
 export type MenuItemProps = {
   currentUrl?: string;
   disabled?: boolean;
-  /**
-   * This is useful for routes that drills into a path,
-   * by still highlighting the original path and not disable "back to start" by click on the same link again
-   * eg. navigating to /collections and clicking on an item to go into /collections/:id and so on
-   */
-  hasPathDrilling?: boolean;
 } & SideBarMenuItem &
   HTMLAttributes<HTMLAnchorElement>;
 
@@ -30,22 +24,16 @@ export const MenuItem = forwardRef<HTMLAnchorElement, MenuItemProps>(
       onClick,
       disabled = false,
       replace = false,
-      hasPathDrilling = false,
     },
     ref
   ) => {
-    const isCurrentUrl = checkUrl({
+    const isActive = isCurrentUrl({
       currentUrl,
       link,
-      replace,
-      hasPathDrilling,
     });
     const { isOpen } = useSideBar();
 
-    const canNavigate = useMemo(
-      () => !disabled && !isCurrentUrl,
-      [disabled, isCurrentUrl]
-    );
+    const canNavigate = useMemo(() => !disabled && !isCurrentUrl, [disabled]);
 
     const handleOnClick = (event: MouseEvent) => {
       if (!canNavigate) {
@@ -59,7 +47,7 @@ export const MenuItem = forwardRef<HTMLAnchorElement, MenuItemProps>(
       return (
         <Link
           to={link}
-          $active={isCurrentUrl}
+          $active={isActive}
           aria-disabled={disabled}
           $disabled={disabled}
           onClick={handleOnClick}
@@ -75,7 +63,7 @@ export const MenuItem = forwardRef<HTMLAnchorElement, MenuItemProps>(
             </IconContainer>
           )}
           <ItemText
-            $active={isCurrentUrl}
+            $active={isActive}
             $disabled={disabled}
             variant="button"
             group="navigation"
@@ -89,7 +77,7 @@ export const MenuItem = forwardRef<HTMLAnchorElement, MenuItemProps>(
     return (
       <Link
         to={link}
-        $active={isCurrentUrl}
+        $active={isActive}
         aria-disabled={disabled}
         $disabled={disabled}
         onClick={handleOnClick}
