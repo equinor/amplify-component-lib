2d94fa1cb3fec09af898678937f42c2d8dbd20d5
Arkadiy Stepanov <arst@equinor.com>
2024-10-15 12:18:27 +0200

    🎨 Expand API of RichTextDisplay

--- src/molecules/RichTextDisplay/RichTextDisplay.tsx
+++ src/molecules/RichTextDisplay/RichTextDisplay.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useRef } from 'react';
+import { FC, HTMLAttributes, useEffect, useRef } from 'react';
 
 import { useEditor } from '@tiptap/react';
 
@@ -11,11 +11,14 @@ export interface RichTextDisplayProps {
   padding?: 'sm' | 'md' | 'lg' | 'none';
 }
 
-export const RichTextDisplay: FC<RichTextDisplayProps> = ({
+export const RichTextDisplay: FC<
+  RichTextDisplayProps & HTMLAttributes<HTMLDivElement>
+> = ({
   value,
   imgReadToken,
   lightBackground = true,
   padding = 'md',
+  ...rest
 }) => {
   const editor = useEditor({
     extensions: [RichText.Kit],
@@ -35,7 +38,11 @@ export const RichTextDisplay: FC<RichTextDisplayProps> = ({
   }, [editor, value]);
 
   return (
-    <RichText.Styling $lightBackground={lightBackground} $padding={padding}>
+    <RichText.Styling
+      $lightBackground={lightBackground}
+      $padding={padding}
+      {...rest}
+    >
       <RichText.Content editor={editor} readOnly />
     </RichText.Styling>
   );
