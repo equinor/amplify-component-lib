2b93e569d452eebd8c60e978cafd2899e50290be
Marius Tobiassen Bungum <marius@mh2.no>
2024-11-20 09:59:23 +0100

    :adhesive_bandage: Fix centering of Status component

--- src/organisms/Status/Status.stories.tsx
+++ src/organisms/Status/Status.stories.tsx
@@ -15,7 +15,7 @@ const meta: Meta = {
   title: 'Organisms/Status',
   component: Status,
   parameters: {
-    layout: 'centered',
+    layout: 'fullscreen',
     design: {
       type: 'figma',
       url: 'https://www.figma.com/design/fk8AI59x5HqPCBg4Nemlkl/%F0%9F%92%A0-Component-Library---Amplify?node-id=9434-152369&m=dev',
