860e0075100c6773c1462aceaa4045d554ec1d82
Arkadiy Stepanov <arst@equinor.com>
2024-11-25 11:29:06 +0100

    🚧 Handle disabled

--- src/organisms/SideBar/CreateItem.tsx
+++ src/organisms/SideBar/CreateItem.tsx
@@ -29,7 +29,7 @@ export const CreateItem: FC<CreateItemProps> = ({
       <MenuItemContainer
         $active={active}
         data-testid="create-item-container"
-        onClick={onCreate}
+        onClick={!disabled ? onCreate : undefined}
       >
         <CreateButton $open variant="contained" disabled={disabled}>
           <Icon data={add} />
@@ -43,7 +43,7 @@ export const CreateItem: FC<CreateItemProps> = ({
       <MenuItemContainer
         $active={active}
         data-testid="create-item-container"
-        onClick={onCreate}
+        onClick={!disabled ? onCreate : undefined}
       >
         <CreateButton variant="contained" disabled={disabled}>
           <Icon data={add} />
