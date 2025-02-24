0b5ed228747e37e434d72de7681edbea6937a448
Marius Tobiassen Bungum <marius@mh2.no>
2025-02-24 12:09:29 +0100

    :mute: Remove console log

--- src/providers/StepperProvider.tsx
+++ src/providers/StepperProvider.tsx
@@ -93,7 +93,6 @@ export const StepperProvider: FC<
 
   const handleOnSetStep = (value: number) => {
     if (rest.syncToURLParam) {
-      console.log(pathWithoutStep);
       navigate(`${pathWithoutStep}/${value}`);
     } else {
       setCurrentStep(value);
