91d764decc93ae34222563e847d0fb44a7ddae77
Marius Tobiassen Bungum <marius@mh2.no>
2025-03-03 13:55:27 +0100

    :adhesive_bandage: Stop calls to impersonate endpoints when in production env

--- src/organisms/TopBar/Account/ImpersonateMenu/hooks/useCanImpersonate.ts
+++ src/organisms/TopBar/Account/ImpersonateMenu/hooks/useCanImpersonate.ts
@@ -2,10 +2,14 @@ import { ImpersonateUserService } from '@equinor/subsurface-app-management';
 import { useQuery } from '@tanstack/react-query';
 
 import { CAN_IMPERSONATE } from '../Impersonate.constants';
+import { environment } from 'src/atoms/utils/auth_environment';
 
 export function useCanImpersonate() {
   return useQuery({
     queryKey: [CAN_IMPERSONATE],
     queryFn: () => ImpersonateUserService.canImpersonate(),
+    enabled:
+      environment.getEnvironmentName(import.meta.env.VITE_ENVIRONMENT_NAME) !==
+      'production',
   });
 }
