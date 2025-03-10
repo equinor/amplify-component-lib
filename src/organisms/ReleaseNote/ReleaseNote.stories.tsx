4bf83265be60fea39cc282a49fb35428c8986621
Marius Tobiassen Bungum <marius@mh2.no>
2025-03-10 08:54:49 +0100

    :sparkles: Created new ReleaseNote component

---
+++ src/organisms/ReleaseNote/ReleaseNote.stories.tsx
@@ -0,0 +1,43 @@
+import { faker } from '@faker-js/faker';
+import { Meta, StoryObj } from '@storybook/react';
+
+import { ReleaseNote } from './ReleaseNote';
+
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  margin: 3rem;
+`;
+
+const meta: Meta<typeof ReleaseNote> = {
+  title: 'Organisms/ReleaseNote',
+  component: ReleaseNote,
+  parameters: {
+    layout: 'centered',
+    design: {
+      type: 'figma',
+      url: '',
+    },
+  },
+  decorators: (Story) => (
+    <Wrapper>
+      <Story />
+    </Wrapper>
+  ),
+  args: {},
+};
+
+export default meta;
+type Story = StoryObj<typeof ReleaseNote>;
+
+export const Default: Story = {
+  args: {
+    releaseId: '1',
+    applicationName: 'PWEX',
+    title: 'Release note title',
+    body: `<p>${faker.lorem.paragraphs(15)}</p>`,
+    releaseDate: new Date('03/09/2024, 10:21').toISOString(),
+    createdDate: new Date('03/09/2024, 10:21').toISOString(),
+    tags: ['Bug fix', 'Improvement', 'Feature'],
+  },
+};
---
+++ src/organisms/ReleaseNote/ToggleExpanded.tsx
@@ -0,0 +1,42 @@
+import { FC } from 'react';
+
+import { Icon } from '@equinor/eds-core-react';
+import { arrow_drop_down, arrow_drop_up } from '@equinor/eds-icons';
+
+import { colors, spacings } from 'src/atoms/style';
+import { Button } from 'src/molecules';
+
+import styled from 'styled-components';
+
+const Container = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  gap: ${spacings.medium};
+  padding: ${spacings.medium};
+  background: linear-gradient(
+    color-mix(in srgb, ${colors.ui.background__default.rgba} 10%, transparent)
+      0%,
+    ${colors.ui.background__default.rgba} 15%
+  );
+  width: 100%;
+`;
+
+interface ToggleExpandedProps {
+  expanded: boolean;
+  onToggleExpanded: () => void;
+}
+
+export const ToggleExpanded: FC<ToggleExpandedProps> = ({
+  expanded,
+  onToggleExpanded,
+}) => {
+  return (
+    <Container>
+      <Button variant="ghost" onClick={onToggleExpanded}>
+        Show {expanded ? 'less' : 'more'}
+        <Icon data={expanded ? arrow_drop_up : arrow_drop_down} />
+      </Button>
+    </Container>
+  );
+};
