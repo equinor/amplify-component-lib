0a8856af7d12bcf7d28cc683ff41dc7baf23b500
Marius Tobiassen Bungum <marius@mh2.no>
2025-03-12 17:30:30 +0100

    :white_check_mark: Create test for quick filter active state

--- src/organisms/Filter/QuickFilter.test.tsx
+++ src/organisms/Filter/QuickFilter.test.tsx
@@ -29,3 +29,36 @@ test('Calls onQuickFilter when selecting value', async () => {
 
   expect(onQuickFilter).toHaveBeenCalledWith(key, randomItem);
 });
+
+test('Sets active like expected', async () => {
+  const items = {
+    [faker.lorem.word()]: new Array(faker.number.int({ min: 1, max: 5 }))
+      .fill(0)
+      .map(() => {
+        return {
+          value: faker.string.uuid(),
+          label: faker.string.uuid(),
+        };
+      }),
+  };
+
+  const key = Object.keys(items)[0];
+  const onQuickFilter = vi.fn();
+  render(
+    <QuickFilter
+      values={{
+        [key]: [items[key][0]],
+      }}
+      items={items}
+      onQuickFilter={onQuickFilter}
+    />
+  );
+
+  const user = userEvent.setup();
+  await user.click(screen.getByRole('button'));
+
+  const item = items[key][0];
+  await user.click(screen.getByRole('menuitem', { name: item.label }));
+
+  expect(onQuickFilter).toHaveBeenCalledWith(key, item);
+});
