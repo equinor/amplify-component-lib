f2706bf902c49f21342962950e05b9eec820858c
Marius Tobiassen Bungum <marius@mh2.no>
2025-03-12 09:56:05 +0100

    :memo: Added favicon to storybook

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,8 +1,15 @@
 import { addons } from '@storybook/manager-api';
 import amplifyTheme from './amplifyTheme';
+import favicon from '../public/favicon-storybook.png';
 
 addons.setConfig({
   theme: amplifyTheme,
   panelPosition: 'right',
   selectedPanel: 'STORYBOOK_ADDON_DESIGNS/panel',
 });
+
+// set favicon for storybook
+const link = document.createElement('link');
+link.setAttribute('rel', 'shortcut icon');
+link.setAttribute('href', favicon);
+document.head.appendChild(link);
