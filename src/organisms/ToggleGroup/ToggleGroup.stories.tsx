c415161556c879fae6523a35c17d7e302cef818b
Marius Tobiassen Bungum <marius@mh2.no>
2025-03-28 11:51:21 +0100

    :sparkles: Created ToggleGroup component

---
+++ src/organisms/ToggleGroup/ToggleGroup.stories.tsx
@@ -0,0 +1,138 @@
+import { ComponentType, FC, useState } from 'react';
+
+import { new_label, person, star_outlined } from '@equinor/eds-icons';
+import { Meta, StoryObj } from '@storybook/react';
+
+import { ToggleGroup as ToggleGroupProps } from './ToggleGroup.types';
+import { ToggleGroup as ToggleGroupComponent } from '.';
+import { ToggleGroupOption } from 'src/organisms/ToggleGroup/ToggleGroupOption';
+
+interface StoryComponentProps {
+  variant: ToggleGroupProps['variant'];
+  withIcons?: boolean;
+  onlyIcons?: boolean;
+  disabled?: boolean;
+}
+
+const ToggleGroup: FC<StoryComponentProps> = ({
+  variant,
+  withIcons = false,
+  onlyIcons = false,
+  disabled = false,
+}) => {
+  const [recentlyPublished, setRecentlyPublished] = useState(false);
+  const [myFiles, setMyFiles] = useState(true);
+  const [favourites, setFavourites] = useState(false);
+
+  return (
+    <ToggleGroupComponent variant={variant}>
+      <ToggleGroupComponent.Option
+        onToggle={setRecentlyPublished}
+        checked={recentlyPublished}
+        disabled={disabled}
+        {...(onlyIcons
+          ? {
+              icon: new_label,
+            }
+          : {
+              label: 'Recently published',
+              icon: withIcons ? new_label : undefined,
+            })}
+      />
+      <ToggleGroupComponent.Option
+        onToggle={setMyFiles}
+        checked={myFiles}
+        {...(onlyIcons
+          ? {
+              icon: person,
+            }
+          : {
+              label: 'My files',
+              icon: withIcons ? person : undefined,
+            })}
+      />
+      <ToggleGroupComponent.Option
+        onToggle={setFavourites}
+        checked={favourites}
+        {...(onlyIcons
+          ? {
+              icon: star_outlined,
+            }
+          : {
+              label: 'Favourites',
+              icon: withIcons ? star_outlined : undefined,
+            })}
+      />
+    </ToggleGroupComponent>
+  );
+};
+
+const meta: Meta = {
+  title: 'Organisms/ToggleGroup',
+  component: ToggleGroup,
+  argTypes: {
+    variant: {
+      control: 'radio',
+      options: ['outlined', 'filled'],
+    },
+    withIcons: {
+      description: 'This is only used in the story',
+      control: 'boolean',
+    },
+  },
+  subcomponents: {
+    'ToggleGroup.Option': ToggleGroupOption as ComponentType<unknown>,
+  },
+  parameters: {
+    layout: 'centered',
+    design: {
+      type: 'figma',
+      url: '',
+    },
+  },
+  args: {
+    withIcons: true,
+  },
+};
+
+export default meta;
+type Story = StoryObj<StoryComponentProps>;
+
+export const Outlined: Story = {
+  args: {
+    variant: 'outlined',
+  },
+};
+
+export const OutlinedWithDisabled: Story = {
+  args: {
+    variant: 'outlined',
+    disabled: true,
+  },
+};
+
+export const Filled: Story = {
+  args: {
+    variant: 'filled',
+  },
+};
+
+export const FilledWithDisabled: Story = {
+  args: {
+    variant: 'filled',
+    disabled: true,
+  },
+};
+
+export const OnlyIconsFilled: Story = {
+  args: {
+    onlyIcons: true,
+    variant: 'filled',
+  },
+};
+export const OnlyIconsOutlined: Story = {
+  args: {
+    onlyIcons: true,
+    variant: 'outlined',
+  },
+};
---
+++ src/organisms/ToggleGroup/ToggleGroup.tsx
@@ -0,0 +1,52 @@
+import { Children, forwardRef } from 'react';
+
+import { ToggleGroup as ToggleGroupProps } from './ToggleGroup.types';
+import { ToggleGroupOption } from './ToggleGroupOption';
+import { colors } from 'src/atoms';
+
+import styled, { css } from 'styled-components';
+
+interface WrapperProps {
+  $variant: Required<ToggleGroupProps>['variant'];
+}
+
+const Wrapper = styled.div<WrapperProps>`
+  display: flex;
+  ${({ $variant }) => {
+    if ($variant === 'outlined') {
+      return css`
+        gap: 1px; // Space for the outline
+        > button {
+          outline: 1px solid ${colors.text.static_icons__tertiary.rgba};
+          &[aria-checked='true'] {
+            outline-color: ${colors.interactive.primary__resting.rgba};
+            &:hover {
+              outline-color: ${colors.interactive.primary__hover.rgba};
+            }
+          }
+          &:disabled {
+            background: none;
+            outline-color: ${colors.interactive.disabled__text.rgba};
+          }
+        }
+      `;
+    }
+  }}
+`;
+
+export const ToggleGroup = forwardRef<HTMLDivElement, ToggleGroupProps>(
+  ({ variant = 'filled', children }, ref) => {
+    return (
+      <Wrapper ref={ref} $variant={variant}>
+        {Children.map(children, (toggleOption) => {
+          if (toggleOption.type != ToggleGroupOption) {
+            throw 'Expected child to be ToggleGroupOption!';
+          }
+          return toggleOption;
+        })}
+      </Wrapper>
+    );
+  }
+);
+
+ToggleGroup.displayName = 'ToggleGroup';
