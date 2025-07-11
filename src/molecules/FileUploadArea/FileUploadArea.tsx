0a2179e67dc08f0ccf22d285b31f5a13c633ab80
Marius Tobiassen Bungum <marius@mh2.no>
2025-07-11 08:19:51 +0200

    :sparkles: Added size prop to FileUploadArea

--- src/molecules/FileUploadArea/FileUploadArea.tsx
+++ src/molecules/FileUploadArea/FileUploadArea.tsx
@@ -4,91 +4,25 @@ import { DropzoneOptions, useDropzone } from 'react-dropzone';
 import { Icon, Typography } from '@equinor/eds-core-react';
 import { folder } from '@equinor/eds-icons';
 
-import { animation, colors, spacings } from 'src/atoms/style';
+import {
+  DividerRow,
+  MediumUploadWrapper,
+  SmallUploadWrapper,
+  UploadWrapper,
+} from './FileUploadArea.styles';
+import { colors } from 'src/atoms/style';
 import { Button, OptionalTooltip } from 'src/molecules';
-import { BORDER_RADIUS } from 'src/molecules/FileProgress/CompactFileProgress.styles';
 import { UploadIllustration } from 'src/molecules/FileUploadArea/UploadIllustration';
 
-import styled from 'styled-components';
-
-const UploadWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  gap: ${spacings.small};
-  justify-content: center;
-  flex-direction: column;
-  min-width: 300px;
-  border: 2px dashed ${colors.interactive.primary__resting.rgba};
-  border-radius: 15px;
-  margin-top: 10px;
-  padding: ${spacings.medium_small} ${spacings.xx_large};
-  /* v8 ignore next */
-  background: ${colors.ui.background__light.rgba};
-  transition: background ${animation.transitionMS};
-  > section {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: ${spacings.x_small};
-    margin: calc(${spacings.medium_small} / 2) 0;
-  }
-`;
-
-const CompactUploadWrapper = styled.div`
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  width: 88px;
-  height: 88px;
-  border: 2px dashed ${colors.interactive.primary__resting.rgba};
-  border-radius: ${BORDER_RADIUS};
-  background: ${colors.ui.background__light.rgba};
-  transition: background ${animation.transitionMS};
-  > svg {
-    width: 48px;
-    height: 48px;
-  }
-  > p {
-    color: ${colors.interactive.primary__resting.rgba};
-  }
-  > p:last-child {
-    color: ${colors.text.static_icons__tertiary.rgba};
-    max-width: 100%;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    text-wrap: nowrap;
-    position: absolute;
-    bottom: calc((1.6em + ${spacings.small}) * -1);
-  }
-  &:hover {
-    cursor: pointer;
-    background: ${colors.interactive.primary__hover_alt.rgba};
-  }
-`;
-
-const DividerRow = styled.div`
-  display: grid;
-  grid-template-columns: 1fr auto 1fr;
-  width: 100%;
-  gap: ${spacings.x_small};
-  align-items: center;
-  > p {
-    color: ${colors.text.static_icons__tertiary.rgba};
-  }
-  > hr {
-    width: 100%;
-    height: 2px;
-  }
-`;
-
 export type FileUploadAreaProps = {
-  compact?: boolean;
+  /*
+   @description Default size is "large"
+   */
+  size?: 'small' | 'medium' | 'large';
 } & DropzoneOptions;
 
 export const FileUploadArea: FC<FileUploadAreaProps> = ({
-  compact,
+  size = 'large',
   ...options
 }) => {
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
@@ -101,18 +35,52 @@ export const FileUploadArea: FC<FileUploadAreaProps> = ({
     }
   }, [options.accept]);
 
-  if (compact) {
+  if (size == 'small') {
     return (
-      <CompactUploadWrapper {...getRootProps()}>
+      <SmallUploadWrapper {...getRootProps()}>
         <input data-testid="file-upload-area-input" {...getInputProps()} />
         <UploadIllustration />
-        <Typography variant="meta">Upload image</Typography>
+        <Typography variant="meta">Upload file</Typography>
         {options.accept && (
           <OptionalTooltip title={`Supported filetypes: ${filetypes}`}>
             <Typography variant="meta">({filetypes})</Typography>
           </OptionalTooltip>
         )}
-      </CompactUploadWrapper>
+      </SmallUploadWrapper>
+    );
+  }
+
+  if (size == 'medium') {
+    return (
+      <MediumUploadWrapper {...getRootProps()}>
+        <input data-testid="file-upload-area-input" {...getInputProps()} />
+        <UploadIllustration />
+        <section>
+          <Typography variant="button" group="navigation">
+            Drag files here
+          </Typography>
+          {options.accept && (
+            <OptionalTooltip title={`Supported filetypes: ${filetypes}`}>
+              <Typography
+                group="paragraph"
+                variant="meta"
+                color={colors.text.static_icons__tertiary.rgba}
+              >
+                {filetypes}
+              </Typography>
+            </OptionalTooltip>
+          )}
+        </section>
+        <DividerRow>
+          <hr />
+          <Typography variant="meta">OR</Typography>
+          <hr />
+        </DividerRow>
+        <Button variant="ghost" onClick={getRootProps().onClick}>
+          <Icon data={folder} />
+          Browse files
+        </Button>
+      </MediumUploadWrapper>
     );
   }
 
