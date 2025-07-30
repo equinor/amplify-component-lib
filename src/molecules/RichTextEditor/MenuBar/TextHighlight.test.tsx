823a398b9fb8c2ca8cd225b0fd30444bd6d992c9
Benjamin Lyon <benjaminlyon0@gmail.com>
2025-07-30 12:14:49 +0200

    ✅ Add test for TextHighlight

---
+++ src/molecules/RichTextEditor/MenuBar/TextHighlight.test.tsx
@@ -0,0 +1,26 @@
+import {
+  RichTextEditor,
+  RichTextEditorFeatures,
+  RichTextEditorProps,
+} from 'src/molecules';
+import { renderWithProviders, screen } from 'src/tests/browsertest-utils';
+
+function fakeProps(): RichTextEditorProps {
+  return {
+    value: `<p>test</p>`,
+    onChange: vi.fn(),
+  };
+}
+
+test('Hides button if not in features', async () => {
+  renderWithProviders(
+    <RichTextEditor
+      {...fakeProps()}
+      removeFeatures={[RichTextEditorFeatures.HIGHLIGHT]}
+    />
+  );
+
+  const input = screen.queryByTestId('highlight-button');
+
+  expect(input).not.toBeInTheDocument();
+});
