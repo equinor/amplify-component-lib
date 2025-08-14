1618484a662d942b14f24b58b43086cc70ee6fa2
aslakihle <aslak.ihle@gmail.com>
2025-08-14 14:24:48 +0200

    :white_check_mark: update button utils test

--- src/molecules/Button/Button.utils.test.ts
+++ src/molecules/Button/Button.utils.test.ts
@@ -4,17 +4,6 @@ test('Returns expected color with icon variant', () => {
   const color = variantAndColorToProgressColor({
     variant: 'contained_icon',
     color: 'primary',
-    disabled: false,
-  });
-
-  expect(color).toBe('neutral');
-});
-
-test('Returns expected color with disabled=true', () => {
-  const color = variantAndColorToProgressColor({
-    variant: 'ghost',
-    color: 'primary',
-    disabled: true,
   });
 
   expect(color).toBe('neutral');
@@ -24,7 +13,6 @@ test('Returns expected color with color=danger', () => {
   const color = variantAndColorToProgressColor({
     variant: 'ghost',
     color: 'danger',
-    disabled: false,
   });
 
   expect(color).toBe('tertiary');
@@ -34,7 +22,6 @@ test('Returns expected color with ghost', () => {
   const color = variantAndColorToProgressColor({
     variant: 'ghost',
     color: undefined,
-    disabled: false,
   });
 
   expect(color).toBe('primary');
