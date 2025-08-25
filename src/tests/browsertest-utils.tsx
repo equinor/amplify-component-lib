d973bd31d88fc13bb57c4dbc7f7b59996d5002f2
Marius Tobiassen Bungum <marius@mh2.no>
2025-08-25 09:26:20 +0200

    :white_check_mark: Created 'renderWithRouter' test util fn

--- src/tests/browsertest-utils.tsx
+++ src/tests/browsertest-utils.tsx
@@ -2,6 +2,13 @@ import { FC, ReactElement, ReactNode } from 'react';
 
 import { faker } from '@faker-js/faker';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import {
+  createMemoryHistory,
+  createRootRoute,
+  createRoute,
+  createRouter,
+  RouterProvider,
+} from '@tanstack/react-router';
 import { render, RenderOptions } from '@testing-library/react';
 import { userEvent } from '@testing-library/user-event';
 import {
@@ -43,6 +50,37 @@ export function fakeSelectItem() {
   };
 }
 
+const renderWithRouter = (
+  ui: ReactElement,
+  routerArgs: {
+    initialEntries: string[];
+    routes: string[];
+    initialIndex?: number;
+  },
+  options?: RenderOptions
+) => {
+  const { initialEntries = ['/'], initialIndex, routes = ['/'] } = routerArgs;
+
+  const rootRoute = createRootRoute();
+
+  const children = routes.map((path: string) =>
+    createRoute({
+      path,
+      getParentRoute: () => rootRoute,
+      component: () => ui,
+    })
+  );
+
+  rootRoute.addChildren(children);
+
+  const router = createRouter({
+    history: createMemoryHistory({ initialEntries, initialIndex }),
+    routeTree: rootRoute,
+  });
+
+  return render(<RouterProvider router={router} />, options);
+};
+
 export function fakeSelectItems(count = 10) {
   return new Array(count).fill(0).map(() => fakeSelectItem());
 }
@@ -81,4 +119,10 @@ export const test = testBase.extend<CustomFixtures>({
 });
 
 // override render method
-export { renderWithProviders, userEvent, vitestBrowserUserEvent, page };
+export {
+  renderWithProviders,
+  renderWithRouter,
+  userEvent,
+  vitestBrowserUserEvent,
+  page,
+};
