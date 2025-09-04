84df349e01c8c1fd834b3bd0c13779eb4d567bdc
Andrés Ignacio Sanhueza Segura <26578864+Rotshrek@users.noreply.github.com>
2025-09-04 10:02:13 +0200

    ✅ IconCell tests

--- src/molecules/IconCell/IconCell.test.tsx
+++ src/molecules/IconCell/IconCell.test.tsx
@@ -18,58 +18,35 @@ function fakeProps(): RegularIconCellProps {
 test('Renders label as expected', () => {
   const props = fakeProps();
   render(<IconCell {...props} />);
+  const cell = screen.getByRole('button');
 
-  expect(screen.getByText(props.label!)).toBeInTheDocument();
+  expect(cell).toBeInTheDocument();
 });
 
 test('Renders icons as expected', () => {
   const props = fakeProps();
-  render(<IconCell {...props} icon={cake} helperIcon={check} />);
+  render(<IconCell {...props} helperIcon={check} />);
   const allIcons = screen.getAllByTestId('eds-icon-path');
+
   expect(allIcons[0]).toHaveAttribute('d', cake.svgPathData);
   expect(allIcons[1]).toHaveAttribute('d', check.svgPathData);
 });
 
 test('Renders custom content as expected', () => {
   const props = fakeProps();
-  const trailingText = 'trailing';
-  const leadingText = 'leading';
+  const helperText = 'helper';
+  const iconText = 'icon';
 
   render(
     <IconCell
       {...props}
-      helperIcon={<div>{trailingText}</div>}
-      icon={<div>{leadingText}</div>}
+      helperIcon={<div>{helperText}</div>}
+      icon={<div>{iconText}</div>}
     />
   );
 
-  expect(screen.getByText(trailingText)).toBeInTheDocument();
-  expect(screen.getByText(leadingText)).toBeInTheDocument();
-});
-
-test('renders icon to left of label', () => {
-  const props = fakeProps();
-  render(<IconCell {...props} icon={cake} />);
-
-  const trailingLabel = screen.getByText(props.label!);
-  const precedingSvg = screen.getByTestId('eds-icon-path')
-    .parentElement as Node;
-
-  const leadingResult = trailingLabel.compareDocumentPosition(precedingSvg);
-
-  expect(leadingResult).toBe(Node.DOCUMENT_POSITION_PRECEDING);
-});
-
-test('renders helperIcon to right of label', () => {
-  const props = fakeProps();
-  render(<IconCell {...props} helperIcon={cake} />);
-
-  const precedingLabel = screen.getByText(props.label!);
-  const trailingSvg = screen.getByTestId('eds-icon-path').parentElement as Node;
-
-  const trailingResult = precedingLabel.compareDocumentPosition(trailingSvg);
-
-  expect(trailingResult).toBe(Node.DOCUMENT_POSITION_FOLLOWING);
+  expect(screen.getByText(helperText)).toBeInTheDocument();
+  expect(screen.getByText(iconText)).toBeInTheDocument();
 });
 
 test('Clicking calls onClick', async () => {
@@ -79,27 +56,9 @@ test('Clicking calls onClick', async () => {
 
   const user = userEvent.setup();
 
-  const container = screen.getByText(props.label!).parentElement!
-    .parentElement!;
+  const cell = screen.getByRole('button');
 
-  await user.click(container);
+  await user.click(cell);
 
   expect(props.onClick).toHaveBeenCalledOnce();
-});
-
-test('OnFocus / OnBlur works', async () => {
-  const props = fakeProps();
-  const onFocus = vi.fn();
-  const onBlur = vi.fn();
-  render(<IconCell {...props} />);
-
-  const user = userEvent.setup();
-
-  await user.tab();
-
-  expect(onFocus).toHaveBeenCalledOnce();
-
-  await user.tab();
-
-  expect(onBlur).toHaveBeenCalledOnce();
 });
