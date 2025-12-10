61d015a8d56c46f5ecced16ff8acb99084287035
Bjorn Ole S. Goa <bjorn.goa@bouvet.no>
2025-12-10 12:11:12 +0100

    :recycle: Refactor StatusAvatar to improve variant handling and color logic

--- src/organisms/TopBar/Account/StatusAvatar.tsx
+++ src/organisms/TopBar/Account/StatusAvatar.tsx
@@ -5,39 +5,58 @@ import { ProfileAvatar } from 'src/molecules/ProfileAvatar/ProfileAvatar';
 
 import styled from 'styled-components';
 
-interface WrapperProps {
-  $variant?: 'impersonate' | 'environment' | 'combined';
+export type StatusVariant = 'impersonate' | 'environment' | 'combined';
+export interface StatusVariantProps {
+  $variant?: StatusVariant;
 }
 
-const Wrapper = styled.div<WrapperProps>`
+const Wrapper = styled.div<StatusVariantProps>`
   width: fit-content;
   border: 2px solid
     ${({ $variant }) => {
       switch ($variant) {
         case 'combined':
-          return colors.interactive.danger__resting.rgba;
+          return colors.interactive.success__resting.rgba;
         case 'environment':
-          return colors.interactive.warning__resting.rgba;
+          return colors.interactive.success__resting.rgba;
         case 'impersonate':
-          return colors.interactive.primary__resting.rgba;
+          return colors.interactive.warning__resting.rgba;
         default:
           return colors.interactive.warning__resting.rgba;
       }
     }};
   border-radius: 50%;
   > div:first-child {
-    background: ${colors.interactive.warning__text.rgba};
+    background: /*${colors.interactive.warning__text.rgba};*/ ${({
+      $variant,
+    }) => {
+      switch ($variant) {
+        case 'combined':
+          return colors.interactive.warning__text.rgba;
+        case 'environment':
+          return colors.interactive.success__text.rgba;
+        case 'impersonate':
+          return colors.interactive.warning__text.rgba;
+        default:
+          return colors.interactive.warning__text.rgba;
+      }
+    }};
   }
 `;
 
 interface StatusAvatarProps {
   size: number;
   name?: string;
+  variant?: StatusVariant;
 }
 
-export const StatusAvatar: FC<StatusAvatarProps> = ({ size, name }) => {
+export const StatusAvatar: FC<StatusAvatarProps> = ({
+  size,
+  name,
+  variant,
+}) => {
   return (
-    <Wrapper>
+    <Wrapper $variant={variant}>
       <ProfileAvatar size={size} name={name ?? 'Environment'} />
     </Wrapper>
   );
