18488c43df457fd4e9a6af2a20c25a617211019d
Bjorn Ole S. Goa <bjorn.goa@bouvet.no>
2025-12-10 07:42:33 +0100

    :recycle: Refactor AccountButton to use StatusAvatar and integrate environment toggle props

--- src/organisms/TopBar/Account/AccountButton.tsx
+++ src/organisms/TopBar/Account/AccountButton.tsx
@@ -2,12 +2,11 @@ import { forwardRef } from 'react';
 
 import { useActiveImpersonationUser } from './ImpersonateMenu/hooks/useActiveImpersonationUser';
 import { ProfileButton } from './Account.styles';
-import { ImpersonateAvatar } from './ImpersonateAvatar';
 import { colors, spacings } from 'src/atoms/style';
+import { SelectOptionRequired } from 'src/molecules';
 import { Chip } from 'src/molecules/Chip/Chip';
 import { ProfileAvatar } from 'src/molecules/ProfileAvatar/ProfileAvatar';
-import { EnvironmentAvatar } from 'src/organisms/TopBar/Account/EnvironmentAvatar';
-import { useToggleActiveEnvironment } from 'src/organisms/TopBar/Account/environmentToggles/hooks/useToggleActiveEnvironment';
+import { StatusAvatar } from 'src/organisms/TopBar/Account/StatusAvatar';
 import { useAuth } from 'src/providers/AuthProvider/AuthProvider';
 
 import styled from 'styled-components';
@@ -18,7 +17,7 @@ const Wrapper = styled.div`
   gap: ${spacings.xx_small};
 `;
 
-const ImpersonationRoleChip = styled(Chip)`
+const StatusChip = styled(Chip)`
   background: ${colors.interactive.warning__resting.rgba};
   outline: none;
   padding: 0;
@@ -26,42 +25,42 @@ const ImpersonationRoleChip = styled(Chip)`
 
 interface AccountButtonProps {
   onClick: () => void;
+  environmentToggle?: SelectOptionRequired[];
 }
 
 export const AccountButton = forwardRef<HTMLButtonElement, AccountButtonProps>(
-  ({ onClick }, ref) => {
+  ({ onClick, environmentToggle }, ref) => {
     const { account, photo } = useAuth();
     const { data: activeImpersonationUser } = useActiveImpersonationUser();
-    const { isActiveFeatureOnCurrentEnvironment, activeFeatures } =
-      useToggleActiveEnvironment();
+    const isActiveFeatureOnCurrentEnvironment =
+      environmentToggle != null && environmentToggle.length > 0;
+    const activeFeatures =
+      environmentToggle == null
+        ? []
+        : environmentToggle.map((item) => item.value);
+
     const impersonationRoles = activeImpersonationUser?.roles?.sort() ?? [];
 
     return (
       <Wrapper>
         {impersonationRoles.at(0) && (
-          <ImpersonationRoleChip>{impersonationRoles[0]}</ImpersonationRoleChip>
+          <StatusChip>{impersonationRoles[0]}</StatusChip>
         )}
         {impersonationRoles.length > 1 && (
-          <ImpersonationRoleChip>
-            {`+${impersonationRoles.length - 1}`}
-          </ImpersonationRoleChip>
+          <StatusChip>{`+${impersonationRoles.length - 1}`}</StatusChip>
         )}
 
-        {activeFeatures && activeFeatures.at(0) && (
-          <ImpersonationRoleChip>
-            {activeFeatures[0].split('-key')[0]}
-          </ImpersonationRoleChip>
+        {isActiveFeatureOnCurrentEnvironment && activeFeatures.at(0) && (
+          <StatusChip>{activeFeatures[0].split('-key')[0]}</StatusChip>
         )}
         {activeFeatures && activeFeatures.length > 1 && (
-          <ImpersonationRoleChip>
-            {`+${activeFeatures.length - 1}`}
-          </ImpersonationRoleChip>
+          <StatusChip>{`+${activeFeatures.length - 1}`}</StatusChip>
         )}
         <ProfileButton ref={ref} onClick={onClick}>
           {isActiveFeatureOnCurrentEnvironment ? (
-            <EnvironmentAvatar size={36} />
+            <StatusAvatar size={36} />
           ) : activeImpersonationUser ? (
-            <ImpersonateAvatar size={36} />
+            <StatusAvatar size={36} />
           ) : (
             <ProfileAvatar size={36} name={account?.name} url={photo} />
           )}
