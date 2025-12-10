205010a64b46075f67604a4592a1a87a3c39be4e
Bjorn Ole S. Goa <bjorn.goa@bouvet.no>
2025-12-10 12:10:08 +0100

    :recycle: Enhance AccountAvatar to support combined status display and environment toggle

--- src/organisms/TopBar/Account/AccountAvatar.tsx
+++ src/organisms/TopBar/Account/AccountAvatar.tsx
@@ -5,7 +5,11 @@ import { colors, spacings } from 'src/atoms/style';
 import { SelectOptionRequired } from 'src/molecules';
 import { Chip } from 'src/molecules/Chip/Chip';
 import { ProfileAvatar } from 'src/molecules/ProfileAvatar/ProfileAvatar';
-import { StatusAvatar } from 'src/organisms/TopBar/Account/StatusAvatar';
+import { impersonateUserDtoToFullName } from 'src/organisms/TopBar/Account/ImpersonateMenu/Impersonate.utils';
+import {
+  StatusAvatar,
+  StatusVariantProps,
+} from 'src/organisms/TopBar/Account/StatusAvatar';
 import { useAuth } from 'src/providers/AuthProvider/AuthProvider';
 
 import styled from 'styled-components';
@@ -15,22 +19,37 @@ const Wrapper = styled.div`
   margin-bottom: ${spacings.small};
 `;
 
-const ImpersonateChip = styled(Chip)`
+const StatusChip = styled(Chip)<StatusVariantProps>`
   position: absolute;
   bottom: calc(${spacings.x_small} * -1);
   left: 50%;
   transform: translateX(-50%);
-  background: ${colors.interactive.warning__resting.rgba};
-  outline-color: ${colors.interactive.warning__resting.rgba};
-`;
-
-const EnvironmentChip = styled(Chip)`
-  position: absolute;
-  bottom: calc(${spacings.x_small} * -1);
-  left: 50%;
-  transform: translateX(-50%);
-  background: ${colors.interactive.warning__resting.rgba};
-  outline-color: ${colors.interactive.warning__resting.rgba};
+  //display: ${({ $variant }) => ($variant == 'combined' ? 'flex' : 'none')};
+  white-space: nowrap;
+  background: ${({ $variant }) => {
+    switch ($variant) {
+      case 'combined':
+        return colors.interactive.warning__resting.rgba;
+      case 'environment':
+        return colors.interactive.success__resting.rgba;
+      case 'impersonate':
+        return colors.interactive.warning__resting.rgba;
+      default:
+        return colors.interactive.warning__resting.rgba;
+    }
+  }};
+  outline-color: ${({ $variant }) => {
+    switch ($variant) {
+      case 'combined':
+        return colors.interactive.success__resting.rgba;
+      case 'environment':
+        return colors.interactive.success__resting.rgba;
+      case 'impersonate':
+        return colors.interactive.warning__resting.rgba;
+      default:
+        return colors.interactive.warning__resting.rgba;
+    }
+  }};
 `;
 
 interface AccountAvatarProps {
@@ -42,6 +61,9 @@ export const AccountAvatar: FC<AccountAvatarProps> = ({
 }) => {
   const { account, photo } = useAuth();
   const { data: activeImpersonationUser } = useActiveImpersonationUser();
+  const fullName = activeImpersonationUser
+    ? impersonateUserDtoToFullName(activeImpersonationUser)
+    : account?.name;
 
   const isActiveFeatureOnCurrentEnvironment =
     environmentToggle != null && environmentToggle.length > 0;
@@ -50,23 +72,43 @@ export const AccountAvatar: FC<AccountAvatarProps> = ({
       ? ''
       : environmentToggle.map((x) => x.label).join(', ');
 
-  if (activeImpersonationUser) {
-    return (
-      <Wrapper>
-        <StatusAvatar size={64} />
-        <ImpersonateChip>Impersonating</ImpersonateChip>
-      </Wrapper>
-    );
-  }
+  const getAvatar = () => {
+    if (isActiveFeatureOnCurrentEnvironment && activeImpersonationUser) {
+      return (
+        <>
+          <StatusAvatar
+            size={64}
+            variant="combined"
+            name="Impersonate & Environment"
+          />
+          <StatusChip $variant="combined">Impersonate & Environment</StatusChip>
+        </>
+      );
+    }
+    if (isActiveFeatureOnCurrentEnvironment) {
+      return (
+        <>
+          <StatusAvatar
+            size={64}
+            variant="environment"
+            name={activeFeatureNames}
+          />
+          <StatusChip $variant="environment">Environment</StatusChip>
+        </>
+      );
+    }
 
-  if (isActiveFeatureOnCurrentEnvironment) {
-    return (
-      <Wrapper>
-        <StatusAvatar size={64} name={activeFeatureNames} />
-        <EnvironmentChip>Environment</EnvironmentChip>
-      </Wrapper>
-    );
-  }
+    if (activeImpersonationUser) {
+      return (
+        <>
+          <StatusAvatar size={64} variant="impersonate" name={fullName} />
+          <StatusChip $variant="impersonate">Impersonate</StatusChip>
+        </>
+      );
+    }
 
-  return <ProfileAvatar size={64} name={account?.name} url={photo} />;
+    return <ProfileAvatar size={64} name={account?.name} url={photo} />;
+  };
+
+  return <Wrapper>{getAvatar()}</Wrapper>;
 };
