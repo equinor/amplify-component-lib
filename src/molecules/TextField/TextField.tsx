5065d8637f63ad2c4349a813143b10c49a23fee2
Marius Tobiassen Bungum <marius@mh2.no>
2026-01-12 12:11:25 +0100

    fix: Uncontrolled init of text field character count

--- src/molecules/TextField/TextField.tsx
+++ src/molecules/TextField/TextField.tsx
@@ -24,7 +24,7 @@ import { SkeletonBase } from 'src/molecules/Skeleton/SkeletonBase/SkeletonBase';
 
 import styled, { css } from 'styled-components';
 
-export type TextFieldProps = Omit<BaseProps, 'variant'> & {
+export type TextFieldProps = Omit<BaseProps, 'variant' | 'inputRef'> & {
   variant?: Variants;
   loading?: boolean;
   maxCharacters?: number;
@@ -182,6 +182,18 @@ export const TextField: FC<TextFieldProps> = (props) => {
     }
   };
 
+  const handleOnTextFieldRender = (
+    element: HTMLInputElement | HTMLTextAreaElement | null
+  ) => {
+    if (
+      props.maxCharacters &&
+      element &&
+      element.value.length !== characterCount
+    ) {
+      setCharacterCount(element.value.length);
+    }
+  };
+
   useEffect(() => {
     if (
       typeof props.value === 'string' &&
@@ -203,6 +215,7 @@ export const TextField: FC<TextFieldProps> = (props) => {
     >
       <Base
         {...baseProps}
+        inputRef={handleOnTextFieldRender}
         disabled={props.loading || props.disabled}
         onChange={handleOnChange as never} // Bypass TS error caused by union of input and textarea attributes
       />
