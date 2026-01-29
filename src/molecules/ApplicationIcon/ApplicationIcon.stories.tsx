4f26bb75a5eb345df967e92bd4ed3e9abf881430
Adrian Lopez Gonzalez <adrian.lg.456@gmail.com>
2026-01-29 11:31:00 +0100

    feat: add Forecast Data Inventory icon and update related components (#1214)

--- src/molecules/ApplicationIcon/ApplicationIcon.stories.tsx
+++ src/molecules/ApplicationIcon/ApplicationIcon.stories.tsx
@@ -19,6 +19,7 @@ const meta: Meta<typeof ApplicationIcon> = {
         'acquire',
         'dasha',
         'forecast-formatter',
+        'fdi',
         'orca',
         'recap',
         'jsembark',
