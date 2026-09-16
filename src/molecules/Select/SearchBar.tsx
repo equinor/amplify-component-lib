82d8d5b1b6c7b627e11045caba02e3d76f18e240
Arkadiy Stepanov <arst@equinor.com>
2026-09-16 08:38:50 +0200

    🚧 Improve disabled handling

--- src/molecules/Select/SearchBar.tsx
+++ src/molecules/Select/SearchBar.tsx
@@ -15,6 +15,7 @@ import {
   ClearButton,
   Container,
   HelperWrapper,
+  LeadingContentWrapper,
   PlaceholderText,
   SearchArea,
   Section,
@@ -152,7 +153,11 @@ export const SearchBar = <T extends SelectOptionRequired>({
         $lightBackground={lightBackground}
       >
         <Section>
-          {!loading && props.leadingContent}
+          {props.leadingContent && (
+            <LeadingContentWrapper $disabled={disabled || loading}>
+              {props.leadingContent}
+            </LeadingContentWrapper>
+          )}
           <SearchArea>
             {!loading && search === '' && selectedValues.length === 0 && (
               <PlaceholderText>{placeholder}</PlaceholderText>
